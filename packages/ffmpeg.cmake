0f8857e7465ce69d2ef2dfa0334b462cebdbb7a6
Ray Griffin <raymond.griffin@protonmail.com>
2021-05-18 13:28:50 +0100

    packages: nuke a load of undeeded stuff.

--- packages/ffmpeg.cmake
+++ packages/ffmpeg.cmake
@@ -1,35 +1,20 @@
 ExternalProject_Add(ffmpeg
     DEPENDS
-        amf-headers
-        avisynth-headers
-        nvcodec-headers
         bzip2
         gmp
         lame
         libressl
-        libssh
         libass
-        libbluray
-        libmodplug
         libpng
-        libsoxr
-        libbs2b
         libvpx
         libwebp
         libzimg
-        libmysofa
         opus
         speex
         vorbis
         x264
-        x265-10bit
         xvidcore
         libxml2
-        libmfx
-        libopenmpt
-        aom
-        dav1d
-        vapoursynth
     GIT_REPOSITORY https://github.com/FFmpeg/FFmpeg.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
@@ -46,41 +31,21 @@ ExternalProject_Add(ffmpeg
         --enable-version3
         --enable-nonfree
         --enable-postproc
-        --enable-avisynth
-        --enable-vapoursynth
         --enable-gmp
         --enable-libass
-        --enable-libbluray
         --enable-libfreetype
         --enable-libfribidi
-        --enable-libmodplug
-        --enable-libopenmpt
         --enable-libmp3lame
         --enable-libopus
-        --enable-libsoxr
         --enable-libspeex
         --enable-libvorbis
-        --enable-libbs2b
         --enable-libvpx
         --enable-libwebp
         --enable-libx264
-        --enable-libx265
-        --enable-libaom
-        --enable-libdav1d
         --enable-libxvid
         --enable-libzimg
         --enable-libtls
         --enable-libxml2
-        --enable-libmysofa
-        --enable-libssh
-        --enable-libmfx
-        --enable-cuda
-        --enable-cuvid
-        --enable-nvdec
-        --enable-nvenc
-        --enable-amf
-        --disable-decoder=libaom_av1
-        "--extra-cflags='-DMODPLUG_STATIC'"
     BUILD_COMMAND ${MAKE}
     INSTALL_COMMAND ${MAKE} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -1,13 +1,10 @@
 ExternalProject_Add(mpv
     DEPENDS
-        angle-headers
         ffmpeg
         fribidi
         lcms2
         libarchive
         libass
-        libdvdnav
-        libdvdread
         libiconv
         libjpeg
         libpng
@@ -15,12 +12,6 @@ ExternalProject_Add(mpv
         rubberband
         uchardet
         openal-soft
-        mujs
-        vulkan
-        shaderc
-        libplacebo
-        spirv-cross
-        vapoursynth
         libsdl2
     GIT_REPOSITORY https://github.com/mpv-player/mpv.git
     UPDATE_COMMAND ""
@@ -30,22 +21,14 @@ ExternalProject_Add(mpv
         DEST_OS=win32
         <SOURCE_DIR>/waf configure
         --enable-static-build
-        --enable-pdf-build
         --disable-manpage-build
-        --enable-libmpv-shared
         --enable-lua
-        --enable-javascript
         --enable-sdl2
         --enable-libarchive
-        --enable-libbluray
-        --enable-dvdnav
         --enable-uchardet
         --enable-rubberband
         --enable-lcms2
         --enable-openal
-        --enable-spirv-cross
-        --enable-vulkan
-        --enable-vapoursynth
         --prefix=${MINGW_INSTALL_PREFIX}
     BUILD_COMMAND ${EXEC} <SOURCE_DIR>/waf
     INSTALL_COMMAND ""
@@ -65,9 +48,7 @@ ExternalProject_Add_Step(mpv strip-binary
     DEPENDEES build
     COMMAND ${EXEC} ${TARGET_ARCH}-objcopy --only-keep-debug <SOURCE_DIR>/build/mpv.exe <SOURCE_DIR>/build/mpv.debug
     COMMAND ${EXEC} ${TARGET_ARCH}-strip -s <SOURCE_DIR>/build/mpv.exe
-    COMMAND ${EXEC} ${TARGET_ARCH}-objcopy --add-gnu-debuglink=<SOURCE_DIR>/build/mpv.debug <SOURCE_DIR>/build/mpv.exe
     COMMAND ${EXEC} ${TARGET_ARCH}-strip -s <SOURCE_DIR>/build/mpv.com
-    COMMAND ${EXEC} ${TARGET_ARCH}-strip -s <SOURCE_DIR>/build/mpv-1.dll
     COMMENT "Stripping mpv binaries"
 )
 
@@ -75,42 +56,6 @@ ExternalProject_Add_Step(mpv copy-binary
     DEPENDEES strip-binary
     COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/build/mpv.exe                     ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv.exe
     COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/build/mpv.com                     ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv.com
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/build/DOCS/man/mpv.pdf            ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/doc/manual.pdf
-    COMMAND ${CMAKE_COMMAND} -E copy ${MINGW_INSTALL_PREFIX}/etc/fonts/fonts.conf   ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv/fonts.conf
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/build/mpv.debug                   ${CMAKE_CURRENT_BINARY_DIR}/mpv-debug/mpv.debug
-
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/build/mpv-1.dll       ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/mpv-1.dll
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/build/mpv.dll.a       ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/libmpv.dll.a
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/build/mpv.def         ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/mpv.def
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/libmpv/client.h       ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/include/client.h
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/libmpv/opengl_cb.h    ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/include/opengl_cb.h
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/libmpv/stream_cb.h    ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/include/stream_cb.h
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/libmpv/render.h       ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/include/render.h
-    COMMAND ${CMAKE_COMMAND} -E copy <SOURCE_DIR>/libmpv/render_gl.h    ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev/include/render_gl.h
-
-    COMMENT "Copying mpv binaries and manual"
-)
-
-set(RENAME ${CMAKE_CURRENT_BINARY_DIR}/mpv-prefix/src/rename.sh)
-file(WRITE ${RENAME}
-"#!/bin/bash
-cd $1
-GIT=$(git rev-parse --short=7 HEAD)
-mv $2 $2-git-\${GIT}")
-
-ExternalProject_Add_Step(mpv copy-package-dir
-    DEPENDEES copy-binary
-    COMMAND chmod 755 ${RENAME}
-    COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/mpv-package ${CMAKE_BINARY_DIR}/mpv-${TARGET_CPU}-${BUILDDATE}
-    COMMAND ${RENAME} <SOURCE_DIR> ${CMAKE_BINARY_DIR}/mpv-${TARGET_CPU}-${BUILDDATE}
-
-    COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/mpv-debug ${CMAKE_BINARY_DIR}/mpv-debug-${TARGET_CPU}-${BUILDDATE}
-    COMMAND ${RENAME} <SOURCE_DIR> ${CMAKE_BINARY_DIR}/mpv-debug-${TARGET_CPU}-${BUILDDATE}
-
-    COMMAND mv ${CMAKE_CURRENT_BINARY_DIR}/mpv-dev ${CMAKE_BINARY_DIR}/mpv-dev-${TARGET_CPU}-${BUILDDATE}
-    COMMAND ${RENAME} <SOURCE_DIR> ${CMAKE_BINARY_DIR}/mpv-dev-${TARGET_CPU}-${BUILDDATE}
-    COMMENT "Moving mpv package folder"
-    LOG 1
 )
 
 force_rebuild_git(mpv)
