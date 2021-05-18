7b9c0796b456d5c3c8d71adfb610d3298bda55de
Ray Griffin <raymond.griffin@protonmail.com>
2021-05-18 17:48:07 +0100

    run.sh: do it slightly differently.

--- run.sh
+++ run.sh
@@ -3,6 +3,5 @@
 pushd build
     ninja update
     ninja mpv
-    cp -v packages/mpv-prefix/src/mpv/build/mpv.exe ../
-    cp -v packages/mpv-prefix/src/mpv/build/mpv.com ../
+    cp -rv packages/mpv-package ../
 popd
