1b4da11b95ecaa1885ea5c8526f739412317c721
rorgoroth <rorgoroth@users.noreply.github.com>
2021-06-07 15:26:35 +0100

    packages/x264: Hardcodes bash
    Also suffers from race condition with make when using too many jobs.

--- packages/x264.cmake
+++ packages/x264.cmake
@@ -2,7 +2,7 @@ ExternalProject_Add(x264
     GIT_REPOSITORY https://code.videolan.org/videolan/x264.git
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} dash <SOURCE_DIR>/configure
+    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
         --host=${TARGET_ARCH}
         --cross-prefix=${TARGET_ARCH}-
         --prefix=${MINGW_INSTALL_PREFIX}
