e200db5be6b34875131a63815ce2bf469f2ac50c
rorgoroth <rorgoroth@users.noreply.github.com>
2021-06-07 13:22:53 +0100

    toolchain/mingw: 8.0.2 --> 9.0.0

--- toolchain/mingw-w64.cmake
+++ toolchain/mingw-w64.cmake
@@ -1,6 +1,7 @@
 ExternalProject_Add(mingw-w64
     PREFIX mingw-w64-prefix
-    URL https://github.com/mirror/mingw-w64/archive/refs/tags/v8.0.2.tar.gz
+    URL https://github.com/mirror/mingw-w64/archive/refs/tags/v9.0.0.tar.gz
+    URL_HASH SHA1=b971369b21d1df8b158eb0a261fbb641054e2fef
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
     INSTALL_COMMAND ""
