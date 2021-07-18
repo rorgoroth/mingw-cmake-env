c76a5048e6deae76b38391616c14e1c532a28807
rorgoroth <rorgoroth@users.noreply.github.com>
2021-07-18 17:37:20 +0100

    run.sh: update

--- run.sh
+++ run.sh
@@ -2,6 +2,8 @@
 
 pushd build
     ninja update
-    ninja -j1 mpv
-    cp -rv packages/mpv-package ../
+    ninja -j1
+    cp -r packages/mpv-package ../bin/
+    cp -r packages/quake3e-package ../bin/
+    cp -r packages/quake3e-urt-package ../bin/
 popd
