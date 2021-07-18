64e2345179aef16e8ba139c78c6f8898b8f80c64
rorgoroth <rorgoroth@users.noreply.github.com>
2021-07-18 21:24:13 +0100

    toolchain/binutils: 2.36.1 -> 2.37

--- toolchain/binutils.cmake
+++ toolchain/binutils.cmake
@@ -1,6 +1,6 @@
 ExternalProject_Add(binutils
-    URL https://ftp.gnu.org/gnu/binutils/binutils-2.36.1.tar.xz
-    URL_HASH MD5=628d490d976d8957279bbbff06cf29d4
+    URL https://ftp.gnu.org/gnu/binutils/binutils-2.37.tar.xz
+    URL_HASH MD5=e78d9ff2976b745a348f4c1f27c77cb1
     CONFIGURE_COMMAND <SOURCE_DIR>/configure
         --target=${TARGET_ARCH}
         --prefix=${CMAKE_INSTALL_PREFIX}
