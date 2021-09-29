f14d3385f6fc266958eb9ce38cccd0a8bc3608b7
Ray Griffin <raymond.griffin@protonmail.com>
2021-09-29 11:33:58 +0100

    run.sh: Update

--- run.sh
+++ run.sh
@@ -1,14 +1,9 @@
-#!/bin/bash
+#!/bin/sh
 
-GREEN='\033[0;32m'
-NC='\033[0m'
-
-pushd build >/dev/null 2>&1 || exit
+cd build || exit
     ninja update && ninja || exit
-    echo -e "${GREEN}>> Deleting old binaries..${NC}"
-    rm -v ../bin/*/*.{exe,com}
-    echo -e "${GREEN}>> ..Done!${NC}" && echo
-    echo -e "${GREEN}>> Copying new binaries..${NC}"
+    rm ../bin/*/*.exe
+    rm ../bin/*/*.com
+    rm ../bin/*/*.dll
     cp -rv packages/*-package ../bin
-    echo -e "${GREEN}>> ..Done!${NC}"
-popd >/dev/null 2>&1 || exit
+cd .. || exit
