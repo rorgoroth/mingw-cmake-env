5c9bded530fc112465f49a961145bbfe33c98301
Ray Griffin <raymond.griffin@protonmail.com>
2021-09-29 11:33:46 +0100

    package.sh: Update

--- package.sh
+++ package.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
 cd bin || exit
-7za a "mpv-$(date +"%Y-%m-%d").7z" mpv-package/*
-7za a "quake3e-package-$(date +"%Y-%m-%d").7z" quake3e-package/*
-7za a "quake3e-urt-package-$(date +"%Y-%m-%d").7z" quake3e-urt-package/*
+
+cd mpv-package || exit
+7za a "mpv-$(date +"%Y-%m-%d").7z" -- ./*
+cd .. || exit
+
+cd quake3e-package || exit
+7za a "quake3e-package-$(date +"%Y-%m-%d").7z" -- ./*
+cd .. || exit
+
+cd quake3e-urt-package || exit
+7za a "quake3e-urt-package-$(date +"%Y-%m-%d").7z" -- ./*
+cd .. || exit
+
+mv -- */*.7z .
+
 cd .. || exit
