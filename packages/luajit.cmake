1aad1a3b6db175cd86008658bd1a47fb486ea90c
rorgoroth <rorgoroth@users.noreply.github.com>
2021-09-30 15:53:19 +0100

    packages/luajit: Clean up

--- packages/luajit.cmake
+++ packages/luajit.cmake
@@ -1,18 +1,3 @@
-if(CYGWIN OR MSYS)
-    # It's much easier to just use the target CC on Cygwin than to worry about
-    # pointer size mismatches
-    set(LUAJIT_HOST_GCC ${TARGET_ARCH}-gcc)
-else()
-    set(LUAJIT_HOST_GCC gcc)
-endif()
-
-if(${TARGET_CPU} MATCHES "i686")
-    set(LUAJIT_GCC_ARGS "-m32")
-else()
-    set(LUAJIT_GCC_ARGS "-m64")
-    set(ENABLE_GC64 "-DLUAJIT_ENABLE_GC64")
-endif()
-
 # luajit ships with a broken pkg-config file
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/luajit.pc.in ${CMAKE_CURRENT_BINARY_DIR}/luajit.pc @ONLY)
 
@@ -25,20 +10,20 @@ ExternalProject_Add(luajit
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ${MAKE}
-        "HOST_CC='${LUAJIT_HOST_GCC} ${LUAJIT_GCC_ARGS}'"
+        "HOST_CC='gcc -m64'"
         CROSS=${TARGET_ARCH}-
         TARGET_SYS=Windows
         BUILDMODE=static
         CFLAGS='-D_WIN32_WINNT=0x0602 -DUNICODE'
-        XCFLAGS='-DLUAJIT_ENABLE_LUA52COMPAT ${ENABLE_GC64}'
+        XCFLAGS='-DLUAJIT_ENABLE_LUA52COMPAT -DLUAJIT_ENABLE_GC64'
     INSTALL_COMMAND ${MAKE}
-        "HOST_CC='${LUAJIT_HOST_GCC} ${LUAJIT_GCC_ARGS}'"
+        "HOST_CC='gcc -m64'"
         CROSS=${TARGET_ARCH}-
         TARGET_SYS=Windows
         BUILDMODE=static
         FILE_T=luajit.exe
         CFLAGS='-D_WIN32_WINNT=0x0602 -DUNICODE'
-        XCFLAGS='-DLUAJIT_ENABLE_LUA52COMPAT ${ENABLE_GC64}'
+        XCFLAGS='-DLUAJIT_ENABLE_LUA52COMPAT -DLUAJIT_ENABLE_GC64'
         PREFIX=${MINGW_INSTALL_PREFIX}
         install
     BUILD_IN_SOURCE 1
