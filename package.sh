1123196ae59f59c4a53505568862ca12f7c5b78d
rorgoroth <rorgoroth@users.noreply.github.com>
2021-10-02 20:56:08 +0100

    package.sh: Fix archive names
    + Delete old

--- package.sh
+++ package.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
 cd bin || exit
+rm -rf ./*.7z
 
 cd mpv-package || exit
 7za a "mpv-$(date +"%Y-%m-%d").7z" -- ./*
 cd .. || exit
 
 cd quake3e-package || exit
-7za a "quake3e-package-$(date +"%Y-%m-%d").7z" -- ./*
+7za a "quake3e-$(date +"%Y-%m-%d").7z" -- ./*
 cd .. || exit
 
 cd quake3e-urt-package || exit
-7za a "quake3e-urt-package-$(date +"%Y-%m-%d").7z" -- ./*
+7za a "quake3e-urt-$(date +"%Y-%m-%d").7z" -- ./*
 cd .. || exit
 
 mv -- */*.7z .
