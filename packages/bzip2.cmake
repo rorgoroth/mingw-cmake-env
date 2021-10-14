5add0260686bfef48e686ba3078776d6d57160f1
rorgoroth <rorgoroth@users.noreply.github.com>
2021-10-14 15:59:36 +0100

    packages/bzip2: Switch to git

--- packages/bzip2.cmake
+++ packages/bzip2.cmake
@@ -1,19 +1,20 @@
 ExternalProject_Add(bzip2
-    URL https://sourceware.org/pub/bzip2/bzip2-1.0.8.tar.gz
-    URL_HASH MD5=67e051268d0c475ea773822f7500d0e5
-    PATCH_COMMAND patch -p1 -i ${CMAKE_CURRENT_SOURCE_DIR}/bzip2-1-fixes.patch
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ${MAKE} libbz2.a
-        PREFIX=${MINGW_INSTALL_PREFIX}
-        CC=${TARGET_ARCH}-gcc
-        AR=${TARGET_ARCH}-ar
-        RANLIB=${TARGET_ARCH}-ranlib
-    INSTALL_COMMAND install -d ${MINGW_INSTALL_PREFIX}/lib
-        COMMAND install -m644 libbz2.a ${MINGW_INSTALL_PREFIX}/lib/
-        COMMAND install -d ${MINGW_INSTALL_PREFIX}/include
-        COMMAND install -m644 bzlib.h ${MINGW_INSTALL_PREFIX}/include/
-    BUILD_IN_SOURCE 1
+    GIT_REPOSITORY https://gitlab.com/bzip2/bzip2.git
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
+        -G Ninja
+        -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
+        -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+        -DCMAKE_BUILD_TYPE=Release
+        -DENABLE_SHARED_LIB=OFF
+        -DENABLE_STATIC_LIB=ON
+        -DENABLE_LIB_ONLY=ON
+        -DENABLE_APP=OFF
+    BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
+    INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
+force_rebuild_git(bzip2)
 extra_step(bzip2)
