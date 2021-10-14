963a5e4a37f19c4e61861f09c82d598082d9e029
rorgoroth <rorgoroth@users.noreply.github.com>
2021-10-14 15:52:12 +0100

    packages/zlib: switch to git

--- packages/zlib.cmake
+++ packages/zlib.cmake
@@ -1,14 +1,17 @@
 ExternalProject_Add(zlib
-    URL https://www.zlib.net/zlib-1.2.11.tar.xz
-    URL_HASH MD5=85adef240c5f370b308da8c938951a68
+    GIT_REPOSITORY https://github.com/madler/zlib.git
+    GIT_REMOTE_NAME origin
+    GIT_TAG develop
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
     PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/zlib-1-win32-static.patch
     CONFIGURE_COMMAND ${EXEC} CHOST=${TARGET_ARCH} <SOURCE_DIR>/configure
         --prefix=${MINGW_INSTALL_PREFIX}
         --static
     BUILD_COMMAND ${MAKE}
-    BUILD_IN_SOURCE 1
     INSTALL_COMMAND ${MAKE} install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
+force_rebuild_git(zlib)
 extra_step(zlib)
