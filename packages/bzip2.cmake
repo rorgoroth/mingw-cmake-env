ca1304716780a24f06f207fef5e2af1f95684962
rorgoroth <rorgoroth@users.noreply.github.com>
2021-10-15 13:45:16 +0100

    packages/bzip2: Fix fontconfig build error, static lib symlink.

--- packages/bzip2.cmake
+++ packages/bzip2.cmake
@@ -9,11 +9,16 @@ ExternalProject_Add(bzip2
         -DCMAKE_BUILD_TYPE=Release
         -DENABLE_SHARED_LIB=OFF
         -DENABLE_STATIC_LIB=ON
-        -DENABLE_LIB_ONLY=ON
+        -DENABLE_LIB_ONLY=OFF
         -DENABLE_APP=OFF
     BUILD_COMMAND ${EXEC} ninja -C <BINARY_DIR>
     INSTALL_COMMAND ${EXEC} ninja -C <BINARY_DIR> install
     LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+ExternalProject_Add_Step(bzip2 symlink
+    DEPENDEES install
+    COMMAND ${CMAKE_COMMAND} -E create_symlink ${MINGW_INSTALL_PREFIX}/lib/libbz2_static.a ${MINGW_INSTALL_PREFIX}/lib/libbz2.a
 )
 
 force_rebuild_git(bzip2)
