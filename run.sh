8386d2b23dfa876b5058dc505f80e215793a2edd
rorgoroth <rorgoroth@users.noreply.github.com>
2021-10-24 21:02:30 +0100

    run.sh: Absorb pkg.sh and package cleaning functionality.

--- run.sh
+++ run.sh
@@ -1,9 +1,52 @@
 #!/bin/sh
 
-cd build || exit
+build () {
+  cd build || exit
     ninja update && ninja || exit
     rm ../bin/*/*.exe
     rm ../bin/*/*.com
     rm ../bin/*/*.dll
     cp -rv packages/*-package ../bin
-cd .. || exit
+  cd .. || exit
+}
+
+package () {
+  cd bin || exit
+  rm -rf ./*.7z
+
+  cd mpv-package || exit
+  7za a "mpv-$(date +"%Y-%m-%d").7z" -- ./*
+  cd .. || exit
+
+  cd quake3e-package || exit
+  7za a "quake3e-$(date +"%Y-%m-%d").7z" -- ./*
+  cd .. || exit
+
+  cd quake3e-urt-package || exit
+  7za a "quake3e-urt-$(date +"%Y-%m-%d").7z" -- ./*
+  cd .. || exit
+
+  mv -- */*.7z .
+
+  cd .. || exit
+}
+
+clean () {
+  rm -rfv build/packages/"$2"-*
+}
+
+case "$1" in
+  "build")
+    build
+    ;;
+  "pkg")
+    package
+    ;;
+  "clean")
+    clean $@
+    ;;
+  *)
+    echo "Accepted Args: build | pkg | clean "package""
+    exit 1
+    ;;
+esac
