1923bff538e63aabb08ff8c555e0ea7fc0436639
Ray Griffin <raymond.griffin@protonmail.com>
2021-10-24 16:30:05 +0100

    toolchain: misc formatting

--- toolchain/binutils.cmake
+++ toolchain/binutils.cmake
@@ -1,33 +1,33 @@
 ExternalProject_Add(binutils
-    URL https://ftp.gnu.org/gnu/binutils/binutils-2.37.tar.xz
-    URL_HASH MD5=e78d9ff2976b745a348f4c1f27c77cb1
-    CONFIGURE_COMMAND <SOURCE_DIR>/configure
-        --target=${TARGET_ARCH}
-        --prefix=${CMAKE_INSTALL_PREFIX}
-        --with-sysroot=${CMAKE_INSTALL_PREFIX}
-        --disable-multilib
-        --disable-nls
-        --disable-shared
-        --disable-win32-registry
-        --without-included-gettext
-        --enable-lto
-        --enable-plugins
-        --enable-threads
-    BUILD_COMMAND make -j${MAKEJOBS}
-    INSTALL_COMMAND make install-strip
-    LOG_DOWNLOAD 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+  URL https://ftp.gnu.org/gnu/binutils/binutils-2.37.tar.xz
+  URL_HASH MD5=e78d9ff2976b745a348f4c1f27c77cb1
+  CONFIGURE_COMMAND <SOURCE_DIR>/configure
+    --target=${TARGET_ARCH}
+    --prefix=${CMAKE_INSTALL_PREFIX}
+    --with-sysroot=${CMAKE_INSTALL_PREFIX}
+    --disable-multilib
+    --disable-nls
+    --disable-shared
+    --disable-win32-registry
+    --without-included-gettext
+    --enable-lto
+    --enable-plugins
+    --enable-threads
+  BUILD_COMMAND make -j${MAKEJOBS}
+  INSTALL_COMMAND make install-strip
+  LOG_DOWNLOAD 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
 
 find_program(PKGCONFIG NAMES pkg-config)
 
 ExternalProject_Add_Step(binutils basedirs
-    DEPENDEES download
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/bin
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${PKGCONFIG} ${CMAKE_INSTALL_PREFIX}/bin/${TARGET_ARCH}-pkg-config
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/${TARGET_ARCH}
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_INSTALL_PREFIX}/${TARGET_ARCH} ${CMAKE_INSTALL_PREFIX}/mingw
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/${TARGET_ARCH}/lib
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_INSTALL_PREFIX}/${TARGET_ARCH}/lib ${CMAKE_INSTALL_PREFIX}/${TARGET_ARCH}/lib64
-    COMMENT "Setting up target directories and symlinks"
+  DEPENDEES download
+  COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}
+  COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/bin
+  COMMAND ${CMAKE_COMMAND} -E create_symlink ${PKGCONFIG} ${CMAKE_INSTALL_PREFIX}/bin/${TARGET_ARCH}-pkg-config
+  COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/${TARGET_ARCH}
+  COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_INSTALL_PREFIX}/${TARGET_ARCH} ${CMAKE_INSTALL_PREFIX}/mingw
+  COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/${TARGET_ARCH}/lib
+  COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_INSTALL_PREFIX}/${TARGET_ARCH}/lib ${CMAKE_INSTALL_PREFIX}/${TARGET_ARCH}/lib64
+  COMMENT "Setting up target directories and symlinks"
 )
--- toolchain/gcc-base.cmake
+++ toolchain/gcc-base.cmake
@@ -1,38 +1,38 @@
 if(${TARGET_CPU} MATCHES "x86_64")
-    set(gcc_arch "x86-64")
-    set(exception "--enable-seh-exceptions")
+  set(gcc_arch "x86-64")
+  set(exception "--enable-seh-exceptions")
 else()
-    set(gcc_arch "i686")
-    set(exception "--enable-dw2-exceptions")
+  set(gcc_arch "i686")
+  set(exception "--enable-dw2-exceptions")
 endif()
 
 ExternalProject_Add(gcc-base
-    DEPENDS
-        mingw-w64-headers
-    PREFIX gcc-prefix
-    STAMP_DIR gcc-prefix/src/gcc-stamp
-    SOURCE_DIR gcc-prefix/src/gcc
-    BINARY_DIR gcc-prefix/src/gcc-build
-    URL https://mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-11.2.0/gcc-11.2.0.tar.gz
-    URL_HASH MD5=dc6886bd44bb49e2d3d662aed9729278
-    CONFIGURE_COMMAND <SOURCE_DIR>/configure
-        --target=${TARGET_ARCH}
-        --prefix=${CMAKE_INSTALL_PREFIX}
-        --libdir=${CMAKE_INSTALL_PREFIX}/lib
-        --with-sysroot=${CMAKE_INSTALL_PREFIX}
-        --disable-multilib
-        --enable-languages=c,c++
-        --disable-nls
-        --disable-shared
-        --disable-win32-registry
-        --with-arch=${gcc_arch}
-        --with-tune=generic
-        --enable-threads=posix
-        --without-included-gettext
-        --enable-lto
-        --enable-checking=release
-        ${exception}
-    BUILD_COMMAND make -j${MAKEJOBS} all-gcc
-    INSTALL_COMMAND make install-strip-gcc
-    LOG_DOWNLOAD 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+  DEPENDS
+    mingw-w64-headers
+  PREFIX gcc-prefix
+  STAMP_DIR gcc-prefix/src/gcc-stamp
+  SOURCE_DIR gcc-prefix/src/gcc
+  BINARY_DIR gcc-prefix/src/gcc-build
+  URL https://mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-11.2.0/gcc-11.2.0.tar.gz
+  URL_HASH MD5=dc6886bd44bb49e2d3d662aed9729278
+  CONFIGURE_COMMAND <SOURCE_DIR>/configure
+    --target=${TARGET_ARCH}
+    --prefix=${CMAKE_INSTALL_PREFIX}
+    --libdir=${CMAKE_INSTALL_PREFIX}/lib
+    --with-sysroot=${CMAKE_INSTALL_PREFIX}
+    --disable-multilib
+    --enable-languages=c,c++
+    --disable-nls
+    --disable-shared
+    --disable-win32-registry
+    --with-arch=${gcc_arch}
+    --with-tune=generic
+    --enable-threads=posix
+    --without-included-gettext
+    --enable-lto
+    --enable-checking=release
+    ${exception}
+  BUILD_COMMAND make -j${MAKEJOBS} all-gcc
+  INSTALL_COMMAND make install-strip-gcc
+  LOG_DOWNLOAD 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
