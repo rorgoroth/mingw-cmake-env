d7bd86f5159f2dcbdba2bfd329791738be63241f
rorgoroth <rorgoroth@users.noreply.github.com>
2021-10-24 16:30:05 +0100

    toolchain: misc formatting

--- toolchain/mingw-w64-headers.cmake
+++ toolchain/mingw-w64-headers.cmake
@@ -1,17 +1,17 @@
 ExternalProject_Add(mingw-w64-headers
-    DEPENDS
-        mingw-w64
-        binutils
-    PREFIX mingw-w64-prefix
-    SOURCE_DIR mingw-w64-prefix/src
-    CONFIGURE_COMMAND <SOURCE_DIR>/mingw-w64/mingw-w64-headers/configure
-        --host=${TARGET_ARCH}
-        --prefix=${MINGW_INSTALL_PREFIX}
-        --enable-sdk=all
-        --enable-secure-api
-        --enable-idl
-        --with-default-win32-winnt=0x600
-    BUILD_COMMAND ""
-    INSTALL_COMMAND make install-strip
-    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+  DEPENDS
+    mingw-w64
+    binutils
+  PREFIX mingw-w64-prefix
+  SOURCE_DIR mingw-w64-prefix/src
+  CONFIGURE_COMMAND <SOURCE_DIR>/mingw-w64/mingw-w64-headers/configure
+    --host=${TARGET_ARCH}
+    --prefix=${MINGW_INSTALL_PREFIX}
+    --enable-sdk=all
+    --enable-secure-api
+    --enable-idl
+    --with-default-win32-winnt=0x600
+  BUILD_COMMAND ""
+  INSTALL_COMMAND make install-strip
+  LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
 )
--- toolchain/mingw-w64.cmake
+++ toolchain/mingw-w64.cmake
@@ -1,9 +1,9 @@
 ExternalProject_Add(mingw-w64
-    PREFIX mingw-w64-prefix
-    URL https://github.com/mirror/mingw-w64/archive/refs/tags/v9.0.0.tar.gz
-    URL_HASH MD5=48a350a4679cb329405b00137dee9809
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    INSTALL_COMMAND ""
-    LOG_DOWNLOAD 1 LOG_UPDATE 1
+  PREFIX mingw-w64-prefix
+  URL https://github.com/mirror/mingw-w64/archive/refs/tags/v9.0.0.tar.gz
+  URL_HASH MD5=48a350a4679cb329405b00137dee9809
+  CONFIGURE_COMMAND ""
+  BUILD_COMMAND ""
+  INSTALL_COMMAND ""
+  LOG_DOWNLOAD 1 LOG_UPDATE 1
 )
