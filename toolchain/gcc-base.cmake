583122af152da561aebda993c8d0a7fe732b19b8
rorgoroth <rorgoroth@users.noreply.github.com>
2021-11-28 16:19:09 +0000

    toolchain: misc updates
    + mingw-{crt,headers} build with ucrt - based upon shinchiro/mpv-winbuild-cmake @ 5d26ca3ea8e26ba9fc2952f5fdd9bda9c02a6f87
    
    + Disable 32bit building, already done on package side just not here yet.

--- toolchain/gcc-base.cmake
+++ toolchain/gcc-base.cmake
@@ -1,11 +1,3 @@
-if(${TARGET_CPU} MATCHES "x86_64")
-  set(gcc_arch "x86-64")
-  set(exception "--enable-seh-exceptions")
-else()
-  set(gcc_arch "i686")
-  set(exception "--enable-dw2-exceptions")
-endif()
-
 ExternalProject_Add(gcc-base
   DEPENDS
     mingw-w64-headers
@@ -21,17 +13,18 @@ ExternalProject_Add(gcc-base
     --libdir=${CMAKE_INSTALL_PREFIX}/lib
     --with-sysroot=${CMAKE_INSTALL_PREFIX}
     --disable-multilib
-    --enable-languages=c,c++
     --disable-nls
     --disable-shared
     --disable-win32-registry
-    --with-arch=${gcc_arch}
-    --with-tune=generic
-    --enable-threads=posix
-    --without-included-gettext
-    --enable-lto
     --enable-checking=release
-    ${exception}
+    --enable-languages=c,c++
+    --enable-libssp
+    --enable-lto
+    --enable-seh-exceptions
+    --enable-threads=posix
+    --with-arch=x86-64
+    --with-tune=generic
+    --without-included-gettext
   BUILD_COMMAND make -j${MAKEJOBS} all-gcc
   INSTALL_COMMAND make install-strip-gcc
   LOG_DOWNLOAD 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
--- toolchain/mingw-w64-headers.cmake
+++ toolchain/mingw-w64-headers.cmake
@@ -1,16 +1,16 @@
 ExternalProject_Add(mingw-w64-headers
   DEPENDS
-    mingw-w64
     binutils
+    mingw-w64
+    widl
   PREFIX mingw-w64-prefix
   SOURCE_DIR mingw-w64-prefix/src
   CONFIGURE_COMMAND <SOURCE_DIR>/mingw-w64/mingw-w64-headers/configure
     --host=${TARGET_ARCH}
     --prefix=${MINGW_INSTALL_PREFIX}
     --enable-sdk=all
-    --enable-secure-api
     --enable-idl
-    --with-default-win32-winnt=0x600
+    --with-default-msvcrt=ucrt
   BUILD_COMMAND ""
   INSTALL_COMMAND make install-strip
   LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
