f3764c2b4f97cb6f266dbc72b3f514959d860df0
Ray Griffin <raymond.griffin@protonmail.com>
2022-02-01 15:37:17 +0000

    packages/toolchain: misc tweaks

--- toolchain/mingw-w64-crt.cmake
+++ toolchain/mingw-w64-crt.cmake
@@ -10,6 +10,8 @@ ExternalProject_Add(mingw-w64-crt
     --with-sysroot=${CMAKE_INSTALL_PREFIX}
     --with-default-msvcrt=msvcrt-os
     --disable-lib32
+    --enable-lib64
+    --enable-wildcard
   BUILD_COMMAND ${MAKE}
   INSTALL_COMMAND ${MAKE} install-strip
   LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
