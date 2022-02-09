e8914edb8a9700a24ba2cd34bd150bdc6e45490a
rorgoroth <rorgoroth@users.noreply.github.com>
2022-02-09 18:36:59 +0000

    toolchain/mingw: Update to snapshot
    08/02/22 https://github.com/mingw-w64/mingw-w64/tree/9ed7b8dcfb3186fdfed1be2a374963458af618b1
    
    Hopefully new release soon, almost 1 year.

--- toolchain/mingw-w64.cmake
+++ toolchain/mingw-w64.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(mingw-w64
   PREFIX mingw-w64-prefix
-  URL https://github.com/mirror/mingw-w64/archive/refs/tags/v9.0.0.tar.gz
-  URL_HASH MD5=48a350a4679cb329405b00137dee9809
+  URL https://github.com/mingw-w64/mingw-w64/archive/9ed7b8dcfb3186fdfed1be2a374963458af618b1.zip
+  URL_HASH MD5=f076bb629c6d88f7ca593ca819710dcf
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
