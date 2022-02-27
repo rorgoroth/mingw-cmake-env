126512446c55cdf47b53ebd0a4797dcc280a96f4
rorgoroth <rorgoroth@users.noreply.github.com>
2022-02-27 15:29:15 +0000

    packages/luajit: Simplify

--- packages/luajit.cmake
+++ packages/luajit.cmake
@@ -1,6 +1,16 @@
 # luajit ships with a broken pkg-config file
 configure_file(${CMAKE_CURRENT_SOURCE_DIR}/luajit.pc.in ${CMAKE_CURRENT_BINARY_DIR}/luajit.pc @ONLY)
 
+set(EXPORT
+    "CROSS=${TARGET_ARCH}-
+    TARGET_SYS=Windows
+    BUILDMODE=static
+    FILE_T=luajit.exe
+    CFLAGS='-D_WIN32_WINNT=0x0A00 -DUNICODE'
+    XCFLAGS='-DLUAJIT_ENABLE_LUA52COMPAT -DLUAJIT_DISABLE_JIT ${ENABLE_GC64}'
+    PREFIX=${MINGW_INSTALL_PREFIX} Q="
+)
+
 ExternalProject_Add(luajit
   DEPENDS libiconv
   GIT_REPOSITORY https://github.com/openresty/luajit2.git
@@ -10,22 +20,10 @@ ExternalProject_Add(luajit
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ${MAKE} -C <SOURCE_DIR>/src
-    "HOST_CC='gcc -m64'"
-    CROSS=${TARGET_ARCH}-
-    TARGET_SYS=Windows
-    BUILDMODE=static
-    CFLAGS='-D_WIN32_WINNT=0x0602 -DUNICODE'
-    XCFLAGS='-DLUAJIT_ENABLE_LUA52COMPAT -DLUAJIT_ENABLE_GC64'
+    ${EXPORT}
     amalg
   INSTALL_COMMAND ${MAKE}
-    "HOST_CC='gcc -m64'"
-    CROSS=${TARGET_ARCH}-
-    TARGET_SYS=Windows
-    BUILDMODE=static
-    FILE_T=luajit.exe
-    CFLAGS='-D_WIN32_WINNT=0x0A00 -DUNICODE'
-    XCFLAGS='-DLUAJIT_ENABLE_LUA52COMPAT -DLUAJIT_ENABLE_GC64'
-    PREFIX=${MINGW_INSTALL_PREFIX}
+    ${EXPORT}
     install
   BUILD_IN_SOURCE 1
   LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
