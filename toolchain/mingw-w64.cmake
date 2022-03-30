dec7ca73171130ff2714c5ae2cc30a7b35392a7f
Ray Griffin <raymond.griffin@protonmail.com>
2022-03-30 18:25:56 +0100

    toolchain/mingw: Update to r6454
    https://github.com/mirror/mingw-w64/tree/b4445ee520dd5041b051fb30ffee00970f50a118

--- toolchain/mingw-w64.cmake
+++ toolchain/mingw-w64.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(mingw-w64
   PREFIX mingw-w64-prefix
-  URL https://github.com/mingw-w64/mingw-w64/archive/99c95def1dcafdd7a2faa671942bdd17cc19864d.zip
-  URL_HASH MD5=6b9ac6ad0098e365ab7b2c8bffa22330
+  URL https://github.com/mirror/mingw-w64/archive/b4445ee520dd5041b051fb30ffee00970f50a118.zip
+  URL_HASH MD5=e7bef8cb7f018cae8859a453149f5ff5
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
