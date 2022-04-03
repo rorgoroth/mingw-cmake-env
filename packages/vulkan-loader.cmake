58eeae1f3d588a22a058c0c75770d78dd465b736
rorgoroth <rorgoroth@users.noreply.github.com>
2022-04-03 21:18:11 +0100

    packages/vulkan-loader: 1.3.209 -> 1.3.210

--- packages/vulkan-loader.cmake
+++ packages/vulkan-loader.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(vulkan-loader
   DEPENDS vulkan-headers
-  URL https://github.com/KhronosGroup/Vulkan-Loader/archive/refs/tags/v1.3.209.tar.gz
-  URL_HASH MD5=9ba3f16bdcd831c463e53e5a5a340807
+  URL https://github.com/KhronosGroup/Vulkan-Loader/archive/refs/tags/v1.3.210.tar.gz
+  URL_HASH MD5=707b552aa6d40d17782ccc8356f4972b
   UPDATE_COMMAND ""
   PATCH_COMMAND ${EXEC} patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/vulkan-loader-0001.patch
         COMMAND ${EXEC} patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/vulkan-loader-0002.patch
@@ -13,7 +13,7 @@ ExternalProject_Add(vulkan-loader
     -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
     -DBUILD_TESTS=OFF
     -DENABLE_STATIC_LOADER=ON
-#    -DENABLE_WERROR=OFF
+    -DENABLE_WERROR=OFF
     -DCMAKE_C_FLAGS='${CMAKE_C_FLAGS} -D_WIN32_WINNT=0x0600 -D__STDC_FORMAT_MACROS -DSTRSAFE_NO_DEPRECATE'
     -DCMAKE_CXX_FLAGS='${CMAKE_CXX_FLAGS} -D__USE_MINGW_ANSI_STDIO -D__STDC_FORMAT_MACROS -fpermissive -D_WIN32_WINNT=0x0600'
   BUILD_COMMAND ${NINJA} -C <BINARY_DIR>
