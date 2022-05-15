32e850fdb56d8c3ef42dad0e308f8b2103e2167a
rorgoroth <rorgoroth@users.noreply.github.com>
2022-05-15 19:52:29 +0100

    packages/mpv: waf -> meson
    NOTE: Requires meson 0.63
    
    Can be installed via pip:
    pip install -U -I --no-deps https://github.com/mesonbuild/meson/releases/download/0.63.0/meson-0.63.0.tar.gz
    
    Don't do ^that^ as root, if you do then you get to keep the pieces, because pip will pollute files managed by the package manager. I do normally keep things as being able to run as root since it's a container but I don't recommend it.
    
    Add pip /bin to your $PATH:
    export PATH="~/.local/bin:$PATH"
    
    I'm going to only push this once 0.63 hits alpines edge/testing repo unless something drastic comes up anyway so this can all be ignored.

--- packages/mpv.cmake
+++ packages/mpv.cmake
@@ -16,47 +16,44 @@ ExternalProject_Add(mpv
     vulkan-loader
   GIT_REPOSITORY https://github.com/mpv-player/mpv.git
   UPDATE_COMMAND ""
-  CONFIGURE_COMMAND ${EXEC}
-    PKG_CONFIG=pkg-config
-    TARGET=${TARGET_ARCH}
-    DEST_OS=win32
-    <SOURCE_DIR>/waf configure
-    --out=<BINARY_DIR>
-    --top=<SOURCE_DIR>
-    --disable-manpage-build
-    --enable-lcms2
-    --enable-lua
-    --enable-rubberband
-    --enable-spirv-cross
-    --enable-static-build
-    --enable-uchardet
-    --enable-vulkan
+  CONFIGURE_COMMAND ${EXEC} meson <BINARY_DIR> <SOURCE_DIR>
     --prefix=${MINGW_INSTALL_PREFIX}
-  BUILD_COMMAND ${EXEC} <SOURCE_DIR>/waf
+    --libdir=${MINGW_INSTALL_PREFIX}/lib
+    --cross-file=${MESON_CROSS}
+    --buildtype=release
+    --default-library=static
+    -Dd3d11=enabled
+    -Diconv=enabled
+    -Djpeg=enabled
+    -Dlcms2=enabled
+    -Dlibplacebo=enabled
+    -Dlua=enabled
+    -Dmanpage-build=disabled
+    -Drubberband=enabled
+    -Dshaderc=enabled
+    -Dspirv-cross=enabled
+    -Duchardet=enabled
+    -Dvulkan=enabled
+    -Dzimg=enabled
+    -Dzlib=enabled
+  BUILD_COMMAND ${NINJA} -C <BINARY_DIR>
   INSTALL_COMMAND ""
   LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
-)
-
-ExternalProject_Add_Step(mpv bootstrap
-  DEPENDEES download
-  DEPENDERS configure
-  COMMAND <SOURCE_DIR>/bootstrap.py
-  WORKING_DIRECTORY <SOURCE_DIR>
-  LOG 1
 )
 
 ExternalProject_Add_Step(mpv strip-binary
   DEPENDEES build
   COMMAND ${EXEC} ${TARGET_ARCH}-strip -s <BINARY_DIR>/mpv.exe
-  COMMAND ${EXEC} ${TARGET_ARCH}-strip -s <BINARY_DIR>/mpv.com
+  COMMAND ${EXEC} ${TARGET_ARCH}-strip -s <BINARY_DIR>/generated/mpv.com
 )
 
 ExternalProject_Add_Step(mpv copy-binary
   DEPENDEES strip-binary
   COMMAND ${CMAKE_COMMAND} -E copy  <BINARY_DIR>/mpv.exe
                                     ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv.exe
-  COMMAND ${CMAKE_COMMAND} -E copy  <BINARY_DIR>/mpv.com
+  COMMAND ${CMAKE_COMMAND} -E copy  <BINARY_DIR>/generated/mpv.com
                                     ${CMAKE_CURRENT_BINARY_DIR}/mpv-package/mpv.com
 )
 
 force_rebuild_git(mpv)
+force_meson_configure(mpv)
