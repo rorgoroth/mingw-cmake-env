d39e679b431c4080290710f5e647c43b3ea10c3d
rorgoroth <rorgoroth@users.noreply.github.com>
2022-06-28 16:52:50 +0100

    toolchain/all: linting & formatting

--- toolchain/mingw-w64-headers.cmake
+++ toolchain/mingw-w64-headers.cmake
@@ -1,11 +1,10 @@
-ExternalProject_Add(mingw-w64-headers
-  DEPENDS
-    binutils
-    mingw-w64
-    widl
+ExternalProject_Add(
+  mingw-w64-headers
+  DEPENDS binutils mingw-w64 widl
   PREFIX mingw-w64-prefix
   SOURCE_DIR mingw-w64-prefix/src
-  CONFIGURE_COMMAND <SOURCE_DIR>/mingw-w64/mingw-w64-headers/configure
+  CONFIGURE_COMMAND
+    <SOURCE_DIR>/mingw-w64/mingw-w64-headers/configure
     --host=${TARGET_ARCH}
     --prefix=${MINGW_INSTALL_PREFIX}
     --enable-idl
@@ -14,5 +13,8 @@ ExternalProject_Add(mingw-w64-headers
     --with-default-win32-winnt=0x0A00
   BUILD_COMMAND ""
   INSTALL_COMMAND make install-strip
-  LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
-)
+  LOG_DOWNLOAD 1
+  LOG_UPDATE 1
+  LOG_CONFIGURE 1
+  LOG_BUILD 1
+  LOG_INSTALL 1)
--- toolchain/winpthreads.cmake
+++ toolchain/winpthreads.cmake
@@ -1,15 +1,16 @@
-ExternalProject_Add(winpthreads
-  DEPENDS
-    mingw-w64-crt
+ExternalProject_Add(
+  winpthreads
+  DEPENDS mingw-w64-crt
   PREFIX mingw-w64-prefix
   SOURCE_DIR mingw-w64-prefix/src
-  CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/mingw-w64/mingw-w64-libraries/winpthreads/configure
+  CONFIGURE_COMMAND
+    ${EXEC} <SOURCE_DIR>/mingw-w64/mingw-w64-libraries/winpthreads/configure
     --host=${TARGET_ARCH}
     --prefix=${MINGW_INSTALL_PREFIX}
     --disable-shared
-    --enable-static
-    CFLAGS='-DWINPTHREAD_STATIC'
+    --enable-static CFLAGS='-DWINPTHREAD_STATIC'
   BUILD_COMMAND ${MAKE}
   INSTALL_COMMAND ${MAKE} install-strip
-  LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
-)
+  LOG_CONFIGURE 1
+  LOG_BUILD 1
+  LOG_INSTALL 1)
