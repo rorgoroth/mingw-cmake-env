a6d695de7f76c8edf0527c350a68e06c92c3c9fa
Ray Griffin <raymond.griffin@protonmail.com>
2022-07-16 17:05:55 +0100

    toolchain/mingw: Update to recent snapshot
    Need a couple of new headers (appmodel) for vulkan-loader.

--- toolchain/mingw-w64.cmake
+++ toolchain/mingw-w64.cmake
@@ -1,8 +1,8 @@
 ExternalProject_Add(
   mingw-w64
   PREFIX mingw-w64-prefix
-  URL https://github.com/mirror/mingw-w64/archive/refs/tags/v10.0.0.tar.gz
-  URL_HASH MD5=8e4bd21fd2349ddb95d2551ff2950a4e
+  URL https://github.com/mirror/mingw-w64/archive/b4116e310dbd1c28868972fd909568f1d9459230.tar.gz
+  URL_HASH MD5=6463ea2c7541b5b6eaeee771e0a13496
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
