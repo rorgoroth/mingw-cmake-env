a9ccb15b5d5d1f6410309f61ad2685c4129e24cc
Ray Griffin <raymond.griffin@protonmail.com>
2022-07-17 06:28:42 +0100

    packages/vulkan: 1.3.219 -> 1.3.221

--- packages/vulkan-loader.cmake
+++ packages/vulkan-loader.cmake
@@ -1,8 +1,8 @@
 ExternalProject_Add(
   vulkan-loader
   DEPENDS vulkan-headers
-  URL https://github.com/KhronosGroup/Vulkan-Loader/archive/refs/tags/v1.3.219.tar.gz
-  URL_HASH MD5=32eeba8f760559a326fad494915f7065
+  URL https://github.com/KhronosGroup/Vulkan-Loader/archive/refs/tags/v1.3.221.tar.gz
+  URL_HASH MD5=44078f35789d9c2520a354aa976b3045
   UPDATE_COMMAND ""
   PATCH_COMMAND ${EXEC} patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/vulkan-loader-0001.patch
         COMMAND ${EXEC} patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/vulkan-loader-0002.patch
@@ -13,7 +13,7 @@ ExternalProject_Add(
     -DVULKAN_HEADERS_INSTALL_DIR=${MINGW_INSTALL_PREFIX}
     -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
     -DBUILD_TESTS=OFF
-    -DENABLE_STATIC_LOADER=ON
+    -DBUILD_STATIC_LOADER=ON
     -DENABLE_WERROR=OFF
     -DCMAKE_C_FLAGS='${CMAKE_C_FLAGS} -D_WIN32_WINNT=0x0A00 -D__STDC_FORMAT_MACROS -DSTRSAFE_NO_DEPRECATE'
     -DCMAKE_CXX_FLAGS='${CMAKE_CXX_FLAGS} -D__USE_MINGW_ANSI_STDIO -D__STDC_FORMAT_MACROS -fpermissive -D_WIN32_WINNT=0x0A00'
