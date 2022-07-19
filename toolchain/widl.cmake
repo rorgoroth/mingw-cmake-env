94e1de7bf16ac0157bff1c34cb15d47202832091
rorgoroth <rorgoroth@users.noreply.github.com>
2022-07-19 14:48:03 +0100

    toolchain+packages: misc
    Mainly logs and any formatting errors.

--- toolchain/widl.cmake
+++ toolchain/widl.cmake
@@ -9,6 +9,8 @@ ExternalProject_Add(
     --target=${TARGET_ARCH}
   BUILD_COMMAND ${MAKE}
   INSTALL_COMMAND ${MAKE} install-strip
+  LOG_DOWNLOAD 1
+  LOG_UPDATE 1
   LOG_CONFIGURE 1
   LOG_BUILD 1
   LOG_INSTALL 1)
