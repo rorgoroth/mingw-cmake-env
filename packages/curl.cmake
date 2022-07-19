55c606fe4ef7791ec1737aa0604329877e94d2fb
Ray Griffin <raymond.griffin@protonmail.com>
2022-07-19 13:09:21 +0100

    packages/curl: raise from the grave
    This reverts commit 55726d854ba12b1f277acc8f545f26d498631419.

---
+++ packages/curl.cmake
@@ -0,0 +1,35 @@
+ExternalProject_Add(curl
+  DEPENDS gcc
+  URL https://github.com/curl/curl/releases/download/curl-7_82_0/curl-7.82.0.tar.bz2
+  URL_HASH MD5=877ca5b5ea2199d37ba357412e7d74be
+  PATCH_COMMAND ${EXEC} patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/curl-001.patch
+  UPDATE_COMMAND ""
+  CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
+    --host=${TARGET_ARCH}
+    --prefix=${MINGW_INSTALL_PREFIX}
+    --disable-curldebug
+    --disable-debug
+    --disable-ldap
+    --disable-ldaps
+    --disable-manual
+    --disable-shared
+    --disable-verbose
+    --disable-warnings
+    --disable-werror
+    --enable-file
+    --enable-ftp
+    --enable-http
+    --enable-ipv6
+    --enable-libcurl-option
+    --enable-optimize
+    --enable-pthreads
+    --enable-silent-rules
+    --enable-static
+    --enable-symbol-hiding
+    --enable-threaded-resolver
+    --with-schannel
+    --without-zlib
+  BUILD_COMMAND ${MAKE}
+  INSTALL_COMMAND ${MAKE} install
+  LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
