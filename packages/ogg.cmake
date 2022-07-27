a002949cb9625dcfbc4498f98cf7192e52c9c3be
Ray Griffin <raymond.griffin@protonmail.com>
2022-07-27 13:30:35 +0100

    packages/ogg: use git

--- packages/ogg.cmake
+++ packages/ogg.cmake
@@ -1,8 +1,7 @@
 ExternalProject_Add(
   ogg
-  URL https://ftp.osuosl.org/pub/xiph/releases/ogg/libogg-1.3.5.tar.xz
-  URL_HASH
-    SHA256=c4d91be36fc8e54deae7575241e03f4211eb102afb3fc0775fbbc1b740016705
+  GIT_REPOSITORY https://github.com/xiph/ogg.git
+  GIT_SHALLOW 1
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND
     ${EXEC} <SOURCE_DIR>/configure
@@ -17,4 +16,5 @@ ExternalProject_Add(
   LOG_BUILD 1
   LOG_INSTALL 1)
 
-autoreconf(ogg)
+force_rebuild_git(ogg)
+autogen(ogg)
