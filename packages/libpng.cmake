2d13bff6c85887ce4d1780055eff0c2ca5d8f958
rorgoroth <rorgoroth@users.noreply.github.com>
2022-08-20 15:12:00 +0100

    packages/libpng: cmake

--- packages/libpng.cmake
+++ packages/libpng.cmake
@@ -5,13 +5,22 @@ ExternalProject_Add(
   GIT_SHALLOW 1
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND
-    ${EXEC} <SOURCE_DIR>/configure
-#    CFLAGS='-fno-asynchronous-unwind-tables'
-    --host=${TARGET_ARCH}
-    --prefix=${MINGW_INSTALL_PREFIX}
-    --disable-shared
-  BUILD_COMMAND ${MAKE}
-  INSTALL_COMMAND ${MAKE} install
+    ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR> -G Ninja
+    -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
+    -DCMAKE_TOOLCHAIN_FILE=${TOOLCHAIN_FILE}
+    -DCMAKE_BUILD_TYPE=Release
+    -DPNG_BUILD_ZLIB=OFF
+    -DPNG_DEBUG=OFF
+    -DPNG_EXECUTABLES=ON
+    -DPNG_FRAMEWORK=OFF
+    -DPNG_HARDWARE_OPTIMIZATIONS=ON
+    -DPNG_INTEL_SSE=on
+    -DPNG_SHARED=OFF
+    -DPNG_STATIC=ON
+    -DPNG_TESTS=OFF
+    -Dld-version-script=ON
+  BUILD_COMMAND ${NINJA} -C <BINARY_DIR>
+  INSTALL_COMMAND ${NINJA} -C <BINARY_DIR> install
   COMMAND
     ${CMAKE_COMMAND} -E create_symlink
     ${MINGW_INSTALL_PREFIX}/bin/libpng-config
@@ -27,4 +36,3 @@ ExternalProject_Add(
   LOG_INSTALL 1)
 
 force_rebuild_git(libpng)
-autoreconf(libpng)
