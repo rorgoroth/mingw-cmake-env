61db10ff0a115b43fe5a948b1f03227ef7c99241
rorgoroth <rorgoroth@users.noreply.github.com>
2022-09-30 17:00:41 +0100

    toolchain/mingw: recent snapshot (Tue, 6 Sep 2022)
    0af130db588b5572f925accc4cbe7001afffe73e

--- toolchain/mingw-w64.cmake
+++ toolchain/mingw-w64.cmake
@@ -1,8 +1,8 @@
 ExternalProject_Add(
   mingw-w64
   PREFIX mingw-w64-prefix
-  URL https://github.com/mirror/mingw-w64/archive/d4a0c84d908243a45255a06dc293d3d7c06db98c.tar.gz
-  URL_HASH MD5=178d802298906ffa5771b737c0bb9433
+  URL https://github.com/mirror/mingw-w64/archive/0af130db588b5572f925accc4cbe7001afffe73e.tar.gz
+  URL_HASH MD5=cd180a95e302b5125805236c49c7c6e5
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
