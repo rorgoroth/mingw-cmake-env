440ffab179c6de17a104f8f15e01fb24861900b0
rorgoroth <rorgoroth@users.noreply.github.com>
2022-10-05 16:46:11 +0100

    packages/xonotic: tweak

--- packages/xonotic.cmake
+++ packages/xonotic.cmake
@@ -2,8 +2,7 @@ ExternalProject_Add(
   xonotic
   DEPENDS gcc
   GIT_REPOSITORY https://github.com/rorgoroth/darkplaces-mingw-w64.git
-  GIT_REMOTE_NAME origin
-  GIT_TAG div0-stable
+  GIT_SHALLOW 1
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ${MAKE} release DP_MAKE_TARGET=mingw WIN64RELEASE=1
@@ -23,9 +22,6 @@ ExternalProject_Add_Step(
     <SOURCE_DIR>/xonotic-dedicated.exe
   COMMAND
     ${EXEC} ${TARGET_ARCH}-strip -s
-    <SOURCE_DIR>/xonotic.exe
-  COMMAND
-    ${EXEC} ${TARGET_ARCH}-strip -s
     <SOURCE_DIR>/xonotic-sdl.exe)
 
 ExternalProject_Add_Step(
@@ -39,10 +35,6 @@ ExternalProject_Add_Step(
     ${CMAKE_COMMAND} -E copy
     <SOURCE_DIR>/xonotic-dedicated.exe
     ${CMAKE_CURRENT_BINARY_DIR}/xonotic-package/xonotic-dedicated.exe
-  COMMAND
-    ${CMAKE_COMMAND} -E copy
-    <SOURCE_DIR>/xonotic.exe
-    ${CMAKE_CURRENT_BINARY_DIR}/xonotic-package/xonotic.exe
   COMMAND
     ${CMAKE_COMMAND} -E copy
     <SOURCE_DIR>/xonotic-sdl.exe
