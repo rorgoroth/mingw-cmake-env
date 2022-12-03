e791e080f43c37ae2ffe54137107936a6837ee3e
rorgoroth <rorgoroth@users.noreply.github.com>
2022-12-03 10:28:42 +0000

    packages/libsdl2: 2.26.0 -> 2.26.1

--- packages/libsdl2.cmake
+++ packages/libsdl2.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(
   libsdl2
-  URL https://github.com/libsdl-org/SDL/releases/download/release-2.26.0/SDL2-2.26.0.tar.gz
-  URL_HASH MD5=35bc58cfe41b8fb6c8e6646be26fa47e
+  URL https://github.com/libsdl-org/SDL/releases/download/release-2.26.1/SDL2-2.26.1.tar.gz
+  URL_HASH MD5=fba211fe2c67609df6fa3cf55d3c74dc
   CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR> -G Ninja
     -DCMAKE_BUILD_TYPE=Release
     -DCMAKE_INSTALL_PREFIX=${MINGW_INSTALL_PREFIX}
