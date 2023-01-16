d8795a06dc9acc9fd72b8a9e695efd2daab4284b
rorgoroth <rorgoroth@users.noreply.github.com>
2023-01-16 16:04:11 +0000

    toolchain/mingw: update... (2023.01.15)
    https://github.com/mirror/mingw-w64/commit/a57cacad31f5766d45959b6c0f9b122918d545fa

--- toolchain/mingw-w64.cmake
+++ toolchain/mingw-w64.cmake
@@ -2,8 +2,8 @@ ExternalProject_Add(
   mingw-w64
   EXCLUDE_FROM_ALL 1
   PREFIX mingw-w64-prefix
-  URL https://github.com/mirror/mingw-w64/archive/c026e9e3c16167f5c675c4a1524be12cc2ab1abc.tar.gz
-  URL_HASH MD5=689f467146ef82d40f9f366dfceff81c
+  URL https://github.com/mirror/mingw-w64/archive/a57cacad31f5766d45959b6c0f9b122918d545fa.tar.gz
+  URL_HASH MD5=f50d6c5dfe03febfba49413425b1bcc5
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND ""
