2aa5b69463cfdc6ade3a3c059b348b941d4d4312
rorgoroth <rorgoroth@users.noreply.github.com>
2023-01-22 18:36:33 +0000

    run.sh: tweak

--- run.sh
+++ run.sh
@@ -4,10 +4,6 @@
 build () {
   cd build || exit
     ninja update && ninja -j1 || exit
-    find ../bin/ -type f -name '*.exe' -delete
-    find ../bin/ -type f -name '*.com' -delete
-    find ../bin/ -type f -name '*.dll' -delete
-    cp -r packages/*-package ../bin
   cd .. || exit
 }
 
@@ -20,12 +16,8 @@ fullbuild () {
     find packages/*-prefix/src/*-stamp/ -maxdepth 1 -type f ! -iname "*.cmake" -size 0c -delete
     ninja update || exit
     ninja clean || exit
-    ninja gcc || exit
-    ninja || exit
-    find ../bin/ -type f -name '*.exe' -delete
-    find ../bin/ -type f -name '*.com' -delete
-    find ../bin/ -type f -name '*.dll' -delete
-    cp -r packages/*-package ../bin
+    ninja -j1 gcc || exit
+    ninja -j1 || exit
   cd .. || exit
 }
 
@@ -33,37 +25,41 @@ fullbuild () {
 package () {
   cd bin || exit
     rm -rf ./*.7z
+    find ./ -type f -name '*.exe' -delete
+    find ./ -type f -name '*.com' -delete
+    find ./ -type f -name '*.dll' -delete
 
-  cd mpv-package || exit
-    7za a "mpv.7z" -- ./*
-  cd .. || exit
+    cp -r ../build/packages/*-package .
 
-  cd ffmpeg-package || exit
-    7za a "ffmpeg.7z" -- ./*
-  cd .. || exit
+    cd mpv-package || exit
+      7za a "mpv.7z" -- ./*
+    cd .. || exit
 
-  cd iortcw-package || exit
-    7za a "iortcw.7z" -- ./*
-  cd .. || exit
+    cd ffmpeg-package || exit
+      7za a "ffmpeg.7z" -- ./*
+    cd .. || exit
 
-  cd quake3e-package || exit
-    7za a "quake3e.7z" -- ./*
-  cd .. || exit
+    cd iortcw-package || exit
+      7za a "iortcw.7z" -- ./*
+    cd .. || exit
 
-  cd quake3e-urbanterror-package || exit
-    7za a "quake3e-urbanterror.7z" -- ./*
-  cd .. || exit
+    cd quake3e-package || exit
+      7za a "quake3e.7z" -- ./*
+    cd .. || exit
 
-  cd quake3e-openarena-package || exit
-    7za a "quake3e-openarena.7z" -- ./*
-  cd .. || exit
+    cd quake3e-urbanterror-package || exit
+      7za a "quake3e-urbanterror.7z" -- ./*
+    cd .. || exit
 
-  cd xonotic-package || exit
-    7za a "xonotic.7z" -- ./*
-  cd .. || exit
+    cd quake3e-openarena-package || exit
+      7za a "quake3e-openarena.7z" -- ./*
+    cd .. || exit
 
-  mv -- */*.7z .
+    cd xonotic-package || exit
+      7za a "xonotic.7z" -- ./*
+    cd .. || exit
 
+    mv -- */*.7z .
   cd .. || exit
 }
 
