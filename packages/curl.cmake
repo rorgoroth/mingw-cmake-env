b7b016704398e6a50f8bf5f3cff264dfeb2763bf
rorgoroth <rorgoroth@users.noreply.github.com>
2023-03-03 20:42:58 +0000

    packages/curl: 7.88.0 -> 7.88.1

--- packages/curl.cmake
+++ packages/curl.cmake
@@ -1,8 +1,8 @@
 ExternalProject_Add(curl
   DEPENDS zlib
           zstd
-  URL https://github.com/curl/curl/releases/download/curl-7_88_0/curl-7.88.0.tar.bz2
-  URL_HASH MD5=9a60d513f7bbb40fc31fc086a3242227
+  URL https://github.com/curl/curl/releases/download/curl-7_88_1/curl-7.88.1.tar.bz2
+  URL_HASH MD5=e90619abb4d275f767e6bfceab5ddabb
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND
     ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR> -G Ninja
