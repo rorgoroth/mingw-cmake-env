a0231e2955e04c7d3d5fdabc757be0ffcef50107
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2023-05-05 13:53:25 +0100

    packages/quake3e-urbanterror: update

--- packages/quake3e-urbanterror.cmake
+++ packages/quake3e-urbanterror.cmake
@@ -21,21 +21,21 @@ ExternalProject_Add_Step(
   DEPENDEES build
   COMMAND
     ${EXEC} ${TARGET_ARCH}-strip -s
-    <SOURCE_DIR>/build/release-mingw64-x86_64/urbanterror-slim.x64.exe
+    <SOURCE_DIR>/build/release-mingw64-x86_64/quake3e.x64.exe
   COMMAND
     ${EXEC} ${TARGET_ARCH}-strip -s
-    <SOURCE_DIR>/build/release-mingw64-x86_64/urbanterror-slim.ded.x64.exe)
+    <SOURCE_DIR>/build/release-mingw64-x86_64/quake3e.ded.x64.exe)
 
 ExternalProject_Add_Step(
   quake3e-urbanterror copy-binary
   DEPENDEES strip-binary
   COMMAND
     ${CMAKE_COMMAND} -E copy
-    <SOURCE_DIR>/build/release-mingw64-x86_64/urbanterror-slim.x64.exe
-    ${CMAKE_CURRENT_BINARY_DIR}/quake3e-urbanterror-package/urbanterror-slim.x64.exe
+    <SOURCE_DIR>/build/release-mingw64-x86_64/quake3e.x64.exe
+    ${CMAKE_CURRENT_BINARY_DIR}/quake3e-urbanterror-package/quake3e.x64.exe
   COMMAND
     ${CMAKE_COMMAND} -E copy
-    <SOURCE_DIR>/build/release-mingw64-x86_64/urbanterror-slim.ded.x64.exe
-    ${CMAKE_CURRENT_BINARY_DIR}/quake3e-urbanterror-package/urbanterror-slim.ded.x64.exe)
+    <SOURCE_DIR>/build/release-mingw64-x86_64/quake3e.ded.x64.exe
+    ${CMAKE_CURRENT_BINARY_DIR}/quake3e-urbanterror-package/quake3e.ded.x64.exe)
 
 force_rebuild_git(quake3e-urbanterror)
