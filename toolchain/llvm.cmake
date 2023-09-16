63b0e16607e18f3d22f9a0db6cc14e00a2c513dd
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2023-09-16 16:32:13 +0100

    toolchain/llvm: cleanup formatting

--- toolchain/llvm.cmake
+++ toolchain/llvm.cmake
@@ -1,3 +1,5 @@
+find_program(PKGCONFIG NAMES pkg-config)
+
 ExternalProject_Add(
   llvm
   EXCLUDE_FROM_ALL 1
@@ -5,45 +7,22 @@ ExternalProject_Add(
   URL_HASH MD5=d32d9e08e05112fb17d4c51c530a1d96
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
-  INSTALL_COMMAND ""
+  INSTALL_COMMAND
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/bin
+    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/bin ${CMAKE_INSTALL_PREFIX}/bin
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/generic-w64-mingw32
+    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/generic-w64-mingw32 ${CMAKE_INSTALL_PREFIX}/generic-w64-mingw32
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/include
+    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include ${CMAKE_INSTALL_PREFIX}/include
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/lib
+    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/lib ${CMAKE_INSTALL_PREFIX}/lib
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/share
+    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/share ${CMAKE_INSTALL_PREFIX}/share
+    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
+    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/x86_64-w64-mingw32 ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
+    COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32 ${CMAKE_INSTALL_PREFIX}/mingw
+    COMMAND ${CMAKE_COMMAND} -E create_symlink ${PKGCONFIG} ${CMAKE_INSTALL_PREFIX}/bin/x86_64-w64-mingw32-pkg-config
+    COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32/lib ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32/lib64
   LOG_DOWNLOAD 1
-  LOG_UPDATE 1
-  LOG_CONFIGURE 1
-  LOG_BUILD 1
-  LOG_INSTALL 1)
-
-find_program(PKGCONFIG NAMES pkg-config)
-
-ExternalProject_Add_Step(
-  llvm basedirs
-  DEPENDEES download
-  COMMAND
-    ${CMAKE_COMMAND} -E make_directory
-    ${CMAKE_INSTALL_PREFIX}
-
-  COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/bin
-  COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/bin ${CMAKE_INSTALL_PREFIX}/bin
-  COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/generic-w64-mingw32
-  COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/generic-w64-mingw32 ${CMAKE_INSTALL_PREFIX}/generic-w64-mingw32
-  COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/include
-  COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include ${CMAKE_INSTALL_PREFIX}/include
-  COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/lib
-  COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/lib ${CMAKE_INSTALL_PREFIX}/lib
-  COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/share
-  COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/share ${CMAKE_INSTALL_PREFIX}/share
-  COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
-  COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/x86_64-w64-mingw32 ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
-
-  COMMAND
-    ${CMAKE_COMMAND} -E create_symlink
-    ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
-    ${CMAKE_INSTALL_PREFIX}/mingw
-  COMMAND
-    ${CMAKE_COMMAND} -E create_symlink
-    ${PKGCONFIG}
-    ${CMAKE_INSTALL_PREFIX}/bin/x86_64-w64-mingw32-pkg-config
-  COMMAND
-    ${CMAKE_COMMAND} -E create_symlink
-    ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32/lib
-    ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32/lib64
-  COMMENT "Setting up target directories and symlinks")
+  LOG_UPDATE 1)
