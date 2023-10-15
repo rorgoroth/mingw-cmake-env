458ff4c188a7f44b09e3e4ad934c6a21c9df40aa
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2023-10-15 11:10:55 +0100

    Revert "packages/quake3e: fix upstream retardation"
    This reverts commit 38fc60887e7f413e0ef024848ab5e4219eef2512.

--- packages/quake3e-openarena.cmake
+++ packages/quake3e-openarena.cmake
@@ -22,7 +22,7 @@ ExternalProject_Add_Step(
   DEPENDEES build
   COMMAND
     ${EXEC} x86_64-w64-mingw32-strip -s
-    <SOURCE_DIR>/build/release-mingw64-x86_64/quake3e.x86_64.exe
+    <SOURCE_DIR>/build/release-mingw64-x86_64/quake3e.x64.exe
   COMMAND
     ${EXEC} x86_64-w64-mingw32-strip -s
     <SOURCE_DIR>/build/release-mingw64-x86_64/quake3e.ded.x64.exe)
@@ -32,7 +32,7 @@ ExternalProject_Add_Step(
   DEPENDEES strip-binary
   COMMAND
     ${CMAKE_COMMAND} -E copy
-    <SOURCE_DIR>/build/release-mingw64-x86_64/quake3e.x86_64.exe
+    <SOURCE_DIR>/build/release-mingw64-x86_64/quake3e.x64.exe
     ${CMAKE_CURRENT_BINARY_DIR}/quake3e-openarena-package/quake3e.x64.exe
   COMMAND
     ${CMAKE_COMMAND} -E copy
