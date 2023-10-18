58f02fef667e02a027670e54ed9db24428dcedef
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2023-10-18 18:52:15 +0100

    toolchain/llvm: 17.0.2 -> 17.0.3

--- toolchain/llvm.cmake
+++ toolchain/llvm.cmake
@@ -3,8 +3,8 @@ find_program(PKGCONFIG NAMES pkg-config)
 ExternalProject_Add(
   llvm
   EXCLUDE_FROM_ALL 1
-  URL https://github.com/rorgoroth/llvm-mingw/releases/download/17.0.2/llvm-x86_64-w64-mingw32_17.0.2.tar.xz
-  URL_HASH MD5=56ff88cd53d2f3ec7ae3f7b960059798
+  URL https://github.com/rorgoroth/llvm-mingw/releases/download/17.0.3/llvm-x86_64-w64-mingw32_17.0.3.tar.xz
+  URL_HASH MD5=3935d61683a8d3077c22d5ebd8e3e710
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND
