2fc5cb1c4b280ef9b0972672fec698e830af3f2b
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2023-10-31 06:07:28 +0000

    packages/quake2pro: patch

--- packages/quake2pro.cmake
+++ packages/quake2pro.cmake
@@ -10,8 +10,9 @@ ExternalProject_Add(
   GIT_REPOSITORY https://github.com/skullernet/q2pro.git
   GIT_SHALLOW 1
   UPDATE_COMMAND ""
+  PATCH_COMMAND ${EXEC} git am ${CMAKE_CURRENT_SOURCE_DIR}/quake2pro-*.patch
   CONFIGURE_COMMAND
-    ${EXEC} meson setup <BINARY_DIR> <SOURCE_DIR>
+    ${EXEC-NLTO} meson setup <BINARY_DIR> <SOURCE_DIR>
     --prefix=${MINGW_INSTALL_PREFIX}
     --libdir=${MINGW_INSTALL_PREFIX}/lib
     --cross-file=${MESON_CROSS}
@@ -27,7 +28,7 @@ ExternalProject_Add(
     -Dsdl2=enabled
     -Dtga=true
     -Dzlib=enabled
-  BUILD_COMMAND ${NINJA} -C <BINARY_DIR>
+  BUILD_COMMAND ${NINJA-NLTO} -C <BINARY_DIR>
   INSTALL_COMMAND ""
   LOG_DOWNLOAD 1
   LOG_UPDATE 1
