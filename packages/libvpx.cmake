f3bc285245e119e2f29ccbfa2a9173ffaed0142a
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2023-11-21 12:10:04 +0000

    packages/libvpx: enable lto

--- packages/libvpx.cmake
+++ packages/libvpx.cmake
@@ -6,7 +6,7 @@ ExternalProject_Add(
   GIT_TAG main
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND
-    ${EXEC-NLTO} CROSS=x86_64-w64-mingw32- <SOURCE_DIR>/configure
+    ${EXEC} CROSS=x86_64-w64-mingw32- <SOURCE_DIR>/configure
     --target=x86_64-win64-gcc
     --prefix=${MINGW_INSTALL_PREFIX}
     --as=yasm
@@ -17,16 +17,15 @@ ExternalProject_Add(
     --disable-shared
     --disable-tools
     --disable-unit-tests
+    --enable-debug
     --enable-postproc
     --enable-runtime-cpu-detect
     --enable-static
     --enable-vp8
     --enable-vp9
     --enable-vp9-postproc
-  BUILD_COMMAND ${MAKE-NLTO}
-  INSTALL_COMMAND ${MAKE-NLTO} install
-    COMMAND ${EXEC-NLTO} x86_64-w64-mingw32-llvm-ranlib
-    ${MINGW_INSTALL_PREFIX}/lib/libvpx.a
+  BUILD_COMMAND ${MAKE}
+  INSTALL_COMMAND ${MAKE} install
   LOG_DOWNLOAD 1
   LOG_UPDATE 1
   LOG_CONFIGURE 1
