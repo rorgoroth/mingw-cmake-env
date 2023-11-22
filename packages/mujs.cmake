cbb0bcfabb2e9a95c9ffa002c0dcd96ef751c7c3
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2023-11-22 07:29:20 +0000

    packages/mujs: 1.3.3 -> 1.3.4

--- packages/mujs.cmake
+++ packages/mujs.cmake
@@ -3,15 +3,16 @@ set(flag
 CC=x86_64-w64-mingw32-clang
 AR=x86_64-w64-mingw32-llvm-ar
 RANLIB=x86_64-w64-mingw32-llvm-ranlib
-OUT=<BINARY_DIR>
 prefix=${MINGW_INSTALL_PREFIX}
+OUT=<BINARY_DIR>
 host=mingw
-VERSION=1.3.3")
+HAVE_READLINE=no
+VERSION=1.3.4")
 
 ExternalProject_Add(
   mujs
-  URL https://github.com/ccxvii/mujs/archive/refs/tags/1.3.3.tar.gz
-  URL_HASH MD5=ba876c5c3bff4ef988b405a148ec111e
+  URL https://github.com/ccxvii/mujs/archive/refs/tags/1.3.4.tar.gz
+  URL_HASH MD5=e72b14da5a91c7229c8b75e76c30f816
   PATCH_COMMAND ${EXEC} patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/mujs-0001.patch
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
