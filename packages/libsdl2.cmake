bc16cbd4c4c3265bf2fa74614b9f007de7166447
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-03-07 21:07:23 +0000

    packages/libsdl2: 2.30.0 -> 2.30.1

--- packages/libsdl2.cmake
+++ packages/libsdl2.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(
   libsdl2
-  URL https://github.com/libsdl-org/SDL/releases/download/release-2.30.0/SDL2-2.30.0.tar.gz
-  URL_HASH MD5=3b0a35e1d794382dcd64d9da93870ea1
+  URL https://github.com/libsdl-org/SDL/releases/download/release-2.30.1/SDL2-2.30.1.tar.gz
+  URL_HASH MD5=a28514e50ef4bb5cc2e1e8c70553425d
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   CONFIGURE_COMMAND ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR> -G Ninja
     -DCMAKE_BUILD_TYPE=Release
