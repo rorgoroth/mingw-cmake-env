e5136bdae0f30c72d3c1f6bceaa8021d69fc702b
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-03-12 16:11:23 +0000

    packages/luajit: misc

--- packages/luajit.cmake
+++ packages/luajit.cmake
@@ -1,17 +1,27 @@
-# luajit ships with a broken pkg-config file
-configure_file(${CMAKE_CURRENT_SOURCE_DIR}/luajit.pc.in
-               ${CMAKE_CURRENT_BINARY_DIR}/luajit.pc @ONLY)
+if(CYGWIN OR MSYS)
+    set(LUAJIT_HOST_GCC ${TARGET_ARCH}-gcc)
+else()
+    set(LUAJIT_HOST_GCC gcc)
+endif()
+
+if(${TARGET_CPU} MATCHES "i686")
+    set(LUAJIT_GCC_ARGS "-m32")
+    set(DISABLE_JIT "-DLUAJIT_DISABLE_JIT")
+else()
+    set(LUAJIT_GCC_ARGS "-m64")
+endif()
 
 set(EXPORT
-    "CROSS=x86_64-w64-mingw32-
+    "CROSS=${TARGET_ARCH}-
     TARGET_SYS=Windows
     BUILDMODE=static
     FILE_T=luajit.exe
     CFLAGS='-D_WIN32_WINNT=0x0A00 -DUNICODE'
-    XCFLAGS='-DLUAJIT_ENABLE_LUA52COMPAT'
-    PREFIX=${MINGW_INSTALL_PREFIX}
-    LDFLAGS='-flto=thin -Wl,--gc-sections'
-    Q=")
+    XCFLAGS='-DLUAJIT_ENABLE_LUA52COMPAT ${DISABLE_JIT}'
+    PREFIX=${MINGW_INSTALL_PREFIX} Q="
+)
+
+configure_file(${CMAKE_CURRENT_SOURCE_DIR}/luajit.pc.in ${CMAKE_CURRENT_BINARY_DIR}/luajit.pc @ONLY)
 
 ExternalProject_Add(
   luajit
@@ -21,9 +31,13 @@ ExternalProject_Add(
   GIT_TAG v2.1-agentzh
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND ${MAKE} -C <SOURCE_DIR>/src ${EXPORT} amalg
-  INSTALL_COMMAND ${MAKE} ${EXPORT} install
-  BUILD_IN_SOURCE 1
+  BUILD_COMMAND ${MAKE} -C <SOURCE_DIR>/src
+                "HOST_CC='${LUAJIT_HOST_GCC} ${LUAJIT_GCC_ARGS}'"
+                ${EXPORT} amalg
+INSTALL_COMMAND ${MAKE}
+                "HOST_CC='${LUAJIT_HOST_GCC} ${LUAJIT_GCC_ARGS}'"
+                ${EXPORT} install
+BUILD_IN_SOURCE 1
   LOG_DOWNLOAD 1
   LOG_UPDATE 1
   LOG_CONFIGURE 1
