9b95b4e8e9936ba1bc2fdc4ac9584dc6527f5566
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-03-27 15:13:09 +0000

    packages/zstd: 1.5.5 -> 1.5.6

--- packages/zstd.cmake
+++ packages/zstd.cmake
@@ -1,8 +1,8 @@
 ExternalProject_Add(
   zstd
   DEPENDS zlib
-  URL https://github.com/facebook/zstd/releases/download/v1.5.5/zstd-1.5.5.tar.gz
-  URL_HASH MD5=63251602329a106220e0a5ad26ba656f
+  URL https://github.com/facebook/zstd/releases/download/v1.5.6/zstd-1.5.6.tar.gz
+  URL_HASH MD5=5a473726b3445d0e5d6296afd1ab6854
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   CONFIGURE_COMMAND
     ${EXEC} meson setup <BINARY_DIR> <SOURCE_DIR>/build/meson
