32f38cb3db18e1487cc391a20e8e168a902642b3
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-04-27 11:51:13 +0100

    packages/fdk-aac: update

--- packages/fdk-aac.cmake
+++ packages/fdk-aac.cmake
@@ -1,18 +1,19 @@
 ExternalProject_Add(fdk-aac
-    DEPENDS gcc
-    GIT_REPOSITORY git://git.code.sf.net/p/opencore-amr/fdk-aac
-    GIT_DEPTH 1
-    UPDATE_COMMAND ""
-    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
-        --host=${TARGET_ARCH}
-        --prefix=${MINGW_INSTALL_PREFIX}
-        --with-sysroot=${CMAKE_INSTALL_PREFIX}
-        --disable-shared
-    BUILD_COMMAND ${MAKE}
-    INSTALL_COMMAND ${MAKE} install
-    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
-)
+  GIT_REPOSITORY https://github.com/mstorsjo/fdk-aac.git
+  GIT_SHALLOW 1
+  UPDATE_COMMAND ""
+  CONFIGURE_COMMAND
+    ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
+    ${cmake_conf_args}
+    -DBUILD_PROGRAMS=OFF
+    -DFDK_AAC_INSTALL_CMAKE_CONFIG_MODULE=ON
+    -DFDK_AAC_INSTALL_PKGCONFIG_MODULE=ON
+    BUILD_COMMAND ${NINJA}
+    INSTALL_COMMAND ${NINJA} install
+    LOG_DOWNLOAD 1
+    LOG_UPDATE 1
+    LOG_CONFIGURE 1
+    LOG_BUILD 1
+    LOG_INSTALL 1)
 
-clean_build_dir(fdk-aac)
 force_rebuild_git(fdk-aac)
-autoreconf(fdk-aac)
