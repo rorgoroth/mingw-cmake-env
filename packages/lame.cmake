2f74fec6447ca8bf2ff1a758feea0b69c86e251f
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-05-26 23:56:30 +0100

    packages/lame: patch

--- packages/lame.cmake
+++ packages/lame.cmake
@@ -1,9 +1,11 @@
 ExternalProject_Add(
   lame
   GIT_REPOSITORY https://gitlab.com/shinchiro/lame.git
+  GIT_SHALLOW 1
+  PATCH_COMMAND ${EXEC} git am -3 ${CMAKE_CURRENT_SOURCE_DIR}/lame-*.patch
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND
-    ${EXEC} autoreconf -fi && <SOURCE_DIR>/configure
+    ${EXEC} autoupdate -f && autoreconf -fi && <SOURCE_DIR>/configure
     ${autotools_conf_args}
     --disable-frontend
   BUILD_COMMAND ${MAKE}
