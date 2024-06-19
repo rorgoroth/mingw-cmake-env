0dac7cb5f5d29f7b597ca0abb3379e5acd6e7257
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-06-19 12:33:43 +0100

    toolchain/llvm: 18.1.7 -> 18.1.8

--- toolchain/llvm.cmake
+++ toolchain/llvm.cmake
@@ -3,27 +3,67 @@ find_program(PKGCONFIG NAMES pkg-config)
 ExternalProject_Add(
   llvm
   EXCLUDE_FROM_ALL 1
-  URL https://github.com/rorgoroth/llvm-mingw/releases/download/18.1.7/18.1.7.tar.zst
-  URL_HASH MD5=f9a21cecb747116147a794e3d27cd8a7
+  URL https://github.com/rorgoroth/llvm-mingw/releases/download/18.1.8/18.1.8.tar.zst
+  URL_HASH MD5=70b0300423a44f74028d0bd9e7e03477
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/bin
-    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/bin ${CMAKE_INSTALL_PREFIX}/bin
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/generic-w64-mingw32
-    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/generic-w64-mingw32 ${CMAKE_INSTALL_PREFIX}/generic-w64-mingw32
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/include
-    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/include ${CMAKE_INSTALL_PREFIX}/include
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/lib
-    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/lib ${CMAKE_INSTALL_PREFIX}/lib
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/share
-    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/share ${CMAKE_INSTALL_PREFIX}/share
-    COMMAND ${CMAKE_COMMAND} -E make_directory ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
-    COMMAND ${CMAKE_COMMAND} -E copy_directory <SOURCE_DIR>/x86_64-w64-mingw32 ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32 ${CMAKE_INSTALL_PREFIX}/mingw
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${PKGCONFIG} ${CMAKE_INSTALL_PREFIX}/bin/x86_64-w64-mingw32-pkg-config
-    COMMAND ${CMAKE_COMMAND} -E create_symlink ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32/lib ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32/lib64
+    COMMAND
+      ${CMAKE_COMMAND} -E make_directory
+      ${CMAKE_INSTALL_PREFIX}
+    COMMAND
+      ${CMAKE_COMMAND} -E make_directory
+      ${CMAKE_INSTALL_PREFIX}/bin
+    COMMAND
+      ${CMAKE_COMMAND} -E copy_directory
+      <SOURCE_DIR>/bin ${CMAKE_INSTALL_PREFIX}/bin
+    COMMAND
+      ${CMAKE_COMMAND} -E make_directory
+      ${CMAKE_INSTALL_PREFIX}/generic-w64-mingw32
+    COMMAND
+      ${CMAKE_COMMAND} -E copy_directory
+      <SOURCE_DIR>/generic-w64-mingw32
+      ${CMAKE_INSTALL_PREFIX}/generic-w64-mingw32
+    COMMAND
+      ${CMAKE_COMMAND} -E make_directory
+      ${CMAKE_INSTALL_PREFIX}/include
+    COMMAND
+      ${CMAKE_COMMAND} -E copy_directory
+      <SOURCE_DIR>/include
+      ${CMAKE_INSTALL_PREFIX}/include
+    COMMAND
+      ${CMAKE_COMMAND} -E make_directory
+      ${CMAKE_INSTALL_PREFIX}/lib
+    COMMAND
+      ${CMAKE_COMMAND} -E copy_directory
+      <SOURCE_DIR>/lib
+      ${CMAKE_INSTALL_PREFIX}/lib
+    COMMAND
+      ${CMAKE_COMMAND} -E make_directory
+      ${CMAKE_INSTALL_PREFIX}/share
+    COMMAND
+      ${CMAKE_COMMAND} -E copy_directory
+      <SOURCE_DIR>/share
+      ${CMAKE_INSTALL_PREFIX}/share
+    COMMAND
+      ${CMAKE_COMMAND} -E make_directory
+      ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
+    COMMAND
+      ${CMAKE_COMMAND} -E copy_directory
+      <SOURCE_DIR>/x86_64-w64-mingw32
+      ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
+    COMMAND
+      ${CMAKE_COMMAND} -E create_symlink
+      ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
+      ${CMAKE_INSTALL_PREFIX}/mingw
+    COMMAND
+      ${CMAKE_COMMAND} -E create_symlink
+      ${PKGCONFIG}
+      ${CMAKE_INSTALL_PREFIX}/bin/x86_64-w64-mingw32-pkg-config
+    COMMAND
+      ${CMAKE_COMMAND} -E create_symlink
+      ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32/lib
+      ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32/lib64
   LOG_DOWNLOAD 1
   LOG_UPDATE 1)
