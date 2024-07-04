08237933d92302658afb231b3d827ecd8ce06f6e
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-07-04 07:44:57 +0100

    packages/zlib: 2.2.0 -> 2.2.0

--- packages/zlib.cmake
+++ packages/zlib.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(
   zlib
-  URL https://github.com/zlib-ng/zlib-ng/archive/refs/tags/2.2.0.tar.gz
-  URL_HASH MD5=b57af52467c821111bed81b663678f7f
+  URL https://github.com/zlib-ng/zlib-ng/archive/refs/tags/2.2.1.tar.gz
+  URL_HASH MD5=026f15ecb1f6d7ef6d31d0c9283c5f02
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND
