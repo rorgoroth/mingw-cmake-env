8864b2f536668ed72daaf2856a4afad293881f0c
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-08-05 18:53:25 +0100

    packages/vulkan: 1.3.290 -> 1.3.292

--- packages/vulkan-headers.cmake
+++ packages/vulkan-headers.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(
   vulkan-headers
-  URL https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/v1.3.290.tar.gz
-  URL_HASH MD5=80774beab5667da772b1819ba5a4e7a6
+  URL https://github.com/KhronosGroup/Vulkan-Headers/archive/refs/tags/v1.3.292.tar.gz
+  URL_HASH MD5=b672b3c28d580e92fc816779d5ceefd0
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND
