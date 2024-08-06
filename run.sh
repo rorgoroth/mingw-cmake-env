333c57efda6b96cee83b47031da61e7286971e51
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-08-06 16:14:55 +0100

    packages/xonotic: removal

--- run.sh
+++ run.sh
@@ -60,10 +60,6 @@ archive() {
         7z a -mx=9 "quake3e-urbanterror.7z" -- ./*
     cd .. || exit
 
-    cd xonotic-package || exit
-        7z a -mx=9 "xonotic.7z" -- ./*
-    cd .. || exit
-
     mv -- */*.7z .
   cd .. || exit
 }
