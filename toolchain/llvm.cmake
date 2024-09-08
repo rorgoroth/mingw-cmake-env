9c0dfbdcca95d518867e656c2ad11c7a4d0e2615
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-09-08 20:11:51 +0100

    toolchain: update

--- toolchain/llvm.cmake
+++ toolchain/llvm.cmake
@@ -3,56 +3,16 @@ find_program(PKGCONFIG NAMES pkg-config)
 ExternalProject_Add(
   llvm
   EXCLUDE_FROM_ALL 1
-  URL https://github.com/rorgoroth/llvm-mingw/releases/download/18.1.8/18.1.8.tar.zst
-  URL_HASH MD5=70b0300423a44f74028d0bd9e7e03477
-  DOWNLOAD_EXTRACT_TIMESTAMP 1
+  GIT_REPOSITORY https://github.com/rorgoroth/llvm-mingw.git
+  GIT_SHALLOW 1
+  UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND ""
+  BUILD_COMMAND "./build.sh"
+  BUILD_IN_SOURCE 1
   INSTALL_COMMAND
     COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}/bin
-    COMMAND
       ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/bin ${CMAKE_INSTALL_PREFIX}/bin
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}/generic-w64-mingw32
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/generic-w64-mingw32
-      ${CMAKE_INSTALL_PREFIX}/generic-w64-mingw32
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}/include
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/include
-      ${CMAKE_INSTALL_PREFIX}/include
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}/lib
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/lib
-      ${CMAKE_INSTALL_PREFIX}/lib
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}/share
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/share
-      ${CMAKE_INSTALL_PREFIX}/share
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/x86_64-w64-mingw32
-      ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
+      <SOURCE_DIR>/build/ ${CMAKE_INSTALL_PREFIX}/
     COMMAND
       ${CMAKE_COMMAND} -E create_symlink
       ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
@@ -66,4 +26,9 @@ ExternalProject_Add(
       ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32/lib
       ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32/lib64
   LOG_DOWNLOAD 1
-  LOG_UPDATE 1)
+  LOG_UPDATE 1
+  LOG_CONFIGURE 1
+  LOG_BUILD 1
+  LOG_INSTALL 1)
+
+force_rebuild_git(llvm)
