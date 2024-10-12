43be8d3aae0a8e74e6697db1704004ca9c3dd599
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-10-12 12:07:14 +0100

    packages/llvm: tweak

--- packages/llvm.cmake
+++ packages/llvm.cmake
@@ -6,12 +6,11 @@ ExternalProject_Add(
   GIT_SHALLOW 1
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND "./build.sh"
+  BUILD_COMMAND
+	COMMAND
+      bash -c "./build-all.sh --host-clang --disable-dylib --disable-lldb --disable-lldb-mi --disable-clang-tools-extra --with-default-win32-winnt=0x0A00 --with-default-msvcrt=ucrt --disable-cfguard ${CMAKE_INSTALL_PREFIX}"
   BUILD_IN_SOURCE 1
   INSTALL_COMMAND
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/build/ ${CMAKE_INSTALL_PREFIX}/
     COMMAND
       ${CMAKE_COMMAND} -E create_symlink
       ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
