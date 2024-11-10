b04e28e6167ac8f6eeffdd3ba2a4c73a8b3d5077
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-11-10 12:37:10 +0000

    packages/expat: 2.6.3 -> 2.6.4

--- packages/expat.cmake
+++ packages/expat.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(
   expat
-  URL https://github.com/libexpat/libexpat/releases/download/R_2_6_3/expat-2.6.3.tar.bz2
-  URL_HASH MD5=c70040a3f2743d9645cb029d3c9a7c89
+  URL https://github.com/libexpat/libexpat/releases/download/R_2_6_4/expat-2.6.4.tar.bz2
+  URL_HASH MD5=f1acfb75967648230d37629e02afbadc
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   CONFIGURE_COMMAND
     ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
