2eceb5e4b34324300fb375c813a880ec5357a848
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2024-12-18 09:05:00 +0000

    packages/llvm: 19.1.5 -> 19.1.6

--- packages/llvm.cmake
+++ packages/llvm.cmake
@@ -3,56 +3,19 @@ find_program(PKGCONFIG NAMES pkg-config)
 ExternalProject_Add(
   llvm
   EXCLUDE_FROM_ALL 1
-  URL https://github.com/rorgoroth/llvm-mingw/releases/download/19.1.5/19.1.5.tar.zst
-  URL_HASH MD5=cd120e4a5b06a94f5dd066a0f14e27b6
+  URL https://github.com/rorgoroth/llvm-mingw/releases/download/19.1.6/19.1.6.tar.zst
+  URL_HASH MD5=c7f3ac11d9854413d23261588222a0b0
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}/bin
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/bin ${CMAKE_INSTALL_PREFIX}/bin
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}/generic-w64-mingw32
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/generic-w64-mingw32
-      ${CMAKE_INSTALL_PREFIX}/generic-w64-mingw32
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}/include
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/include
-      ${CMAKE_INSTALL_PREFIX}/include
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}/lib
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/lib
-      ${CMAKE_INSTALL_PREFIX}/lib
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}/share
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/share
-      ${CMAKE_INSTALL_PREFIX}/share
-    COMMAND
-      ${CMAKE_COMMAND} -E make_directory
-      ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
-    COMMAND
-      ${CMAKE_COMMAND} -E copy_directory
-      <SOURCE_DIR>/x86_64-w64-mingw32
-      ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
+    COMMAND sh -c "mkdir -p ${CMAKE_INSTALL_PREFIX}"
+    COMMAND sh -c "cp -ar <SOURCE_DIR>/bin ${CMAKE_INSTALL_PREFIX}/"
+    COMMAND sh -c "cp -ar <SOURCE_DIR>/generic-w64-mingw32 ${CMAKE_INSTALL_PREFIX}/"
+    COMMAND sh -c "cp -ar <SOURCE_DIR>/include ${CMAKE_INSTALL_PREFIX}/"
+    COMMAND sh -c "cp -ar <SOURCE_DIR>/lib ${CMAKE_INSTALL_PREFIX}/"
+    COMMAND sh -c "cp -ar <SOURCE_DIR>/share ${CMAKE_INSTALL_PREFIX}/"
+    COMMAND sh -c "cp -ar <SOURCE_DIR>/x86_64-w64-mingw32 ${CMAKE_INSTALL_PREFIX}/"
     COMMAND
       ${CMAKE_COMMAND} -E create_symlink
       ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32
@@ -66,4 +29,5 @@ ExternalProject_Add(
       ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32/lib
       ${CMAKE_INSTALL_PREFIX}/x86_64-w64-mingw32/lib64
   LOG_DOWNLOAD 1
+  LOG_INSTALL 1
   LOG_UPDATE 1)
