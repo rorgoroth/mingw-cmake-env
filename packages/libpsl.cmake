394f426f00c20486de2862ae60c85c9c6120a26a
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2025-02-05 19:26:42 +0000

    packages/libpsl: add package

---
+++ packages/libpsl.cmake
@@ -0,0 +1,18 @@
+ExternalProject_Add(
+  libpsl
+  GIT_REPOSITORY https://github.com/rockdaboot/libpsl.git
+  GIT_SHALLOW 1
+  UPDATE_COMMAND ""
+  CONFIGURE_COMMAND
+    ${EXEC} <SOURCE_DIR>/autogen.sh && <SOURCE_DIR>/configure
+    ${autotools_conf_args}
+  BUILD_COMMAND ${MAKE}
+  INSTALL_COMMAND ${MAKE} install
+  BUILD_IN_SOURCE 1
+  LOG_DOWNLOAD 1
+  LOG_UPDATE 1
+  LOG_CONFIGURE 1
+  LOG_BUILD 1
+  LOG_INSTALL 1)
+
+force_rebuild_git(libpsl)
