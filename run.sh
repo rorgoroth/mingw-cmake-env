e5e4172b0434e345317b87966d489ffe1eb590c8
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2025-05-30 15:44:14 +0100

    run.sh: update

--- run.sh
+++ run.sh
@@ -2,32 +2,42 @@
 
 # Standard build
 build() {
-    ninja -C build update || exit
-    ninja -C build llvm || exit
-    ninja -C build || exit
+  ninja -C build update || exit
+  ninja -C build llvm || exit
+  ninja -C build quake2pro || exit
+  ninja -C build quake3e || exit
+  ninja -C build quake3e-openarena || exit
+  ninja -C build quake3e-urbanterror || exit
+  ninja -C build xonotic || exit
+  ninja -C build ffmpeg || exit
+  ninja -C build mpv || exit
 }
 
 # Full build, clean up as much as possible. You should manually
 # remove the installation prefix before running this as to create
 # a clean environment.
 fullbuild() {
-    find build/toolchain/*-prefix/src/*-stamp/ -maxdepth 1 -type f ! -iname "*.cmake" -size 0c -delete
-    find build/packages/*-prefix/src/*-stamp/ -maxdepth 1 -type f ! -iname "*.cmake" -size 0c -delete
-    ninja -C build update || exit
-    ninja -C build clean || exit
-    ninja -C build llvm || exit
-    ninja -C build || exit
+  ninja -C build clean || exit
+  ninja -C build update || exit
+  ninja -C build llvm || exit
+  ninja -C build quake2pro || exit
+  ninja -C build quake3e || exit
+  ninja -C build quake3e-openarena || exit
+  ninja -C build quake3e-urbanterror || exit
+  ninja -C build xonotic || exit
+  ninja -C build ffmpeg || exit
+  ninja -C build mpv || exit
 }
 
 # Copy package contents in place.
 package() {
-  cd bin || exit
-    rm -rf ./*.7z
-    find ./ -type f -name '*.exe' -delete
-    find ./ -type f -name '*.com' -delete
-    find ./ -type f -name '*.dll' -delete
-    cp -r ../build/packages/*-package .
-  cd .. || exit
+ cd bin || exit
+   rm -rf ./*.7z
+   find ./ -type f -name '*.exe' -delete
+   find ./ -type f -name '*.com' -delete
+   find ./ -type f -name '*.dll' -delete
+   cp -r ../build/packages/*-package .
+ cd .. || exit
 }
 
 # Create package archives
@@ -71,20 +81,20 @@ archive() {
 
 # Upload packages
 release() {
-    gh release delete latest -y
-    git tag --delete latest
-    git push --delete origin latest
-    gh release create -t "Latest Build" -n "Latest Build" latest ./bin/*.7z
+  gh release delete latest -y
+  git tag --delete latest
+  git push --delete origin latest
+  gh release create -t "Latest Build" -n "Latest Build" latest ./bin/*.7z
 }
 
 # Clean a package, useful for random build failures.
 clean() {
-    rm -rf build/packages/"$2"-*
+  rm -rf build/packages/"$2"-*
 }
 
 # Run update checking script
 checkupdates() {
-    sh ./packages/update-check.sh | column -t
+  sh ./packages/update-check.sh | column -t
 }
 
 case "$1" in
