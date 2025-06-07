938bf87e8dd3a631811780e3e4fc3f52e22084b0
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2025-06-07 20:41:30 +0100

    packages/sqlite: 3.45.3 -> 3.50.1

--- packages/sqlite.cmake
+++ packages/sqlite.cmake
@@ -1,11 +1,11 @@
 ExternalProject_Add(
   sqlite
-  URL https://www.sqlite.org/2024/sqlite-autoconf-3450300.tar.gz
-  URL_HASH MD5=f418272ba1d588747dcd4201018ed912
+  URL https://www.sqlite.org/2025/sqlite-autoconf-3500100.tar.gz
+  URL_HASH MD5=45508b1aeb0a946574d3385249c5fb58
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND
-    ${EXEC} autoreconf -fi && <SOURCE_DIR>/configure
+    ${EXEC} <SOURCE_DIR>/configure
     ${autotools_conf_args}
   BUILD_COMMAND ${MAKE}
   INSTALL_COMMAND ${MAKE} install
