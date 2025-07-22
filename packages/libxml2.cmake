206a7aa9a8e821e2853b42ff75124c33955954df
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2025-07-22 14:25:51 +0100

    packages/libxml2: 2.14.4 -> 2.14.4

--- packages/libxml2.cmake
+++ packages/libxml2.cmake
@@ -2,8 +2,8 @@ ExternalProject_Add(
   libxml2
   DEPENDS libiconv
           zlib
-  URL https://github.com/GNOME/libxml2/archive/refs/tags/v2.14.4.tar.gz
-  URL_HASH MD5=a2a59d8ffd868c892c53e295e192db10
+  URL https://github.com/GNOME/libxml2/archive/refs/tags/v2.14.5.tar.gz
+  URL_HASH MD5=58e0afb9b166716ca5ee4a41fdb7d138
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   CONFIGURE_COMMAND
     ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
