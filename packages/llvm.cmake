5c2caeb2a9be21a52cda4e93a10945a6f770d6a9
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2025-09-24 08:37:24 +0100

    packages/llvm: 21.1.1 -> 21.1.2

--- packages/llvm.cmake
+++ packages/llvm.cmake
@@ -3,8 +3,8 @@ find_program(PKGCONFIG NAMES pkg-config)
 ExternalProject_Add(
   llvm
   EXCLUDE_FROM_ALL 1
-  URL https://github.com/rorgoroth/llvm-mingw/releases/download/21.1.1/21.1.1.tar.zst
-  URL_HASH SHA256=56bc92369b4a4bc163930d10a906b4fd8119716b500be45627024d2a2039f422
+  URL https://github.com/rorgoroth/llvm-mingw/releases/download/21.1.2/21.1.2.tar.zst
+  URL_HASH SHA256=060fbc663a0f88b7a738aa37aa7ee0a83101cd12a8eb857e16600b17fb614fad
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
