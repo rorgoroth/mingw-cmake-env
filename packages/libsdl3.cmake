a4a5b9c7cbbeb257b57f7d3b7c0e9af68c23d77f
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2025-11-01 06:17:58 +0000

    packages/libsdl3: 3.2.24 -> 3.2.26

--- packages/libsdl3.cmake
+++ packages/libsdl3.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(
   libsdl3
-  URL https://github.com/libsdl-org/SDL/archive/refs/tags/release-3.2.24.tar.gz
-  URL_HASH MD5=4ab30a0de706066999baab267db9f68e
+  URL https://github.com/libsdl-org/SDL/archive/refs/tags/release-3.2.26.tar.gz
+  URL_HASH MD5=07b4fde84a508f64cc3d076caac70f55
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   CONFIGURE_COMMAND
     ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
