d24f1214cc47932fd11b545e1d4530868002b96a
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2025-11-10 14:18:54 +0000

    packages/harfbuzz: 12.1.0 -> 12.2.0

--- packages/harfbuzz.cmake
+++ packages/harfbuzz.cmake
@@ -2,8 +2,8 @@ ExternalProject_Add(
   harfbuzz
   DEPENDS freetype2
           libpng
-  URL https://github.com/harfbuzz/harfbuzz/archive/refs/tags/12.1.0.tar.gz
-  URL_HASH MD5=435777e0862ed08a2b237a09fedbdbd6
+  URL https://github.com/harfbuzz/harfbuzz/archive/refs/tags/12.2.0.tar.gz
+  URL_HASH MD5=57b6b75fdd9319b51e0a11f8f43228b0
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND
