bb21b5697f5aa764c3b3cde5ae4e3c57a5f27426
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2026-02-26 15:35:01 +0000

    packages/mujs: fix build...
    Doesn't like clang 22's LTO, silent lto module error causing bad lib, breaking mpv linking.

--- packages/mujs.cmake
+++ packages/mujs.cmake
@@ -3,18 +3,18 @@ set(flags
 OUT=<BINARY_DIR>
 host=mingw
 HAVE_READLINE=no
-VERSION=1.3.7")
+VERSION=1.3.8")
 
 ExternalProject_Add(
   mujs
   URL https://codeberg.org/ccxvii/mujs/archive/1.3.8.tar.gz
   URL_HASH MD5=55dd114344d4e4cb739328a29c60378f
   DOWNLOAD_EXTRACT_TIMESTAMP 1
-  PATCH_COMMAND ${EXEC} patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/mujs-0001.patch
+  PATCH_COMMAND patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/mujs-0001.patch
   UPDATE_COMMAND ""
   CONFIGURE_COMMAND ""
-  BUILD_COMMAND ${MAKE} ${flags}
-  INSTALL_COMMAND ${MAKE} ${flags} install-static
+  BUILD_COMMAND ${MAKE-NLTO} ${flags}
+  INSTALL_COMMAND ${MAKE-NLTO} ${flags} install-static
   BUILD_IN_SOURCE 1
   LOG_DOWNLOAD 1
   LOG_UPDATE 1
