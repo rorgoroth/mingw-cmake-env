ff46d153d3d68ffc3e65a6a7c361d543cccfd403
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2026-06-02 20:56:31 +0100

    packages/sdl3: 3.4.8 -> 3.4.10

--- packages/sdl3.cmake
+++ packages/sdl3.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(
   sdl3
-  URL https://github.com/libsdl-org/SDL/archive/refs/tags/release-3.4.8.tar.gz
-  URL_HASH MD5=349b657759caf69a8ec8a866fab1e19c
+  URL https://github.com/libsdl-org/SDL/archive/refs/tags/release-3.4.10.tar.gz
+  URL_HASH MD5=1f9aab29041d6434c9fe5986bbe62992
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   CONFIGURE_COMMAND
     ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
