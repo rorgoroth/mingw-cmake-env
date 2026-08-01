819d90c82cf552b91756b138dcdb1219c9d5314c
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2026-08-01 13:07:13 +0100

    packages/fontconfig: 2.18.1 -> 2.18.2

--- packages/fontconfig.cmake
+++ packages/fontconfig.cmake
@@ -5,8 +5,8 @@ ExternalProject_Add(
           freetype2
           libiconv
           zlib
-  URL https://gitlab.freedesktop.org/api/v4/projects/890/packages/generic/fontconfig/2.18.1/fontconfig-2.18.1.tar.xz
-  URL_HASH MD5=7c28095f89559d9bdef9d2e5d9d882f6
+  URL https://gitlab.freedesktop.org/api/v4/projects/890/packages/generic/fontconfig/2.18.2/fontconfig-2.18.2.tar.xz
+  URL_HASH MD5=784b516220f93441d8ffc67ee9b3b34a
   PATCH_COMMAND ${EXEC} patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/fontconfig-0001.patch
   CONFIGURE_COMMAND
     ${EXEC} meson setup --reconfigure <BINARY_DIR> <SOURCE_DIR>
