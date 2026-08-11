34a34d52f4893a49fbe30e868eb19f15c9a1d912
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2026-08-11 15:40:44 +0100

    packages/expat: 2.8.2 -> 2.8.3

--- packages/expat.cmake
+++ packages/expat.cmake
@@ -1,7 +1,7 @@
 ExternalProject_Add(
   expat
-  URL https://github.com/libexpat/libexpat/releases/download/R_2_8_2/expat-2.8.2.tar.bz2
-  URL_HASH MD5=666d3631746d921fd074822f7027c5fe
+  URL https://github.com/libexpat/libexpat/releases/download/R_2_8_3/expat-2.8.3.tar.bz2
+  URL_HASH MD5=5dec87c0bdb90952a51696665484aed9
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   CONFIGURE_COMMAND
     ${EXEC} cmake -H<SOURCE_DIR> -B<BINARY_DIR>
