8047599fba37c297478b769940a00956df881fbc
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2026-08-20 11:55:55 +0100

    packages/vulkan: 1.4.359 -> 1.4.360

--- packages/vulkan-loader.cmake
+++ packages/vulkan-loader.cmake
@@ -1,8 +1,8 @@
 ExternalProject_Add(
   vulkan-loader
   DEPENDS vulkan-headers
-  URL https://github.com/KhronosGroup/Vulkan-Loader/archive/refs/tags/v1.4.359.tar.gz
-  URL_HASH MD5=59438511117b86326cdcefb92f1a1fe2
+  URL https://github.com/KhronosGroup/Vulkan-Loader/archive/refs/tags/v1.4.360.tar.gz
+  URL_HASH MD5=43bab8749bd3e963e40a4ac1a06b63c0
   DOWNLOAD_EXTRACT_TIMESTAMP 1
   UPDATE_COMMAND ""
   PATCH_COMMAND ${EXEC} patch -p1 < ${CMAKE_CURRENT_SOURCE_DIR}/vulkan-loader-0001.patch
