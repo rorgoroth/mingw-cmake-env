72af1959f44ca60470d7a8a4462409badac5ec38
rorgoroth <77244135+rorgoroth@users.noreply.github.com>
2026-09-06 12:17:41 +0100

    packages/update-check: update sdl3 check
    Forgot to change this on last sdl3 update.

--- packages/update-check.sh
+++ packages/update-check.sh
@@ -71,7 +71,7 @@ check
 
 # sdl3
 pkg=sdl3
-a=$(cat ./packages/sdl3.cmake | grep 'release-' | sed -n 's,.*release-\([0-9][^>]*\)\.tar.*,\1,p')
+a=$(cat ./packages/sdl3.cmake | grep 'release-' | sed -n 's,.*release-\([0-9.]*\)/.*,\1,p')
 b=$(wget -q -O- 'https://github.com/libsdl-org/SDL/tags' | grep 'href="/libsdl-org/SDL/archive/refs/tags' | grep 'release-3' | sed -n 's,.*href="/libsdl-org/SDL/archive/refs/tags/release-\([0-9][^"_]*\)\.tar.*,\1,p' | sort -Vr | head -1)
 check
 
